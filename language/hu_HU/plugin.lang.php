6541a3b6f55f6c5dc92ef0152c8201d2cef9f15b
ddtddt <ddtddt@piwigo.org>
2009-11-15 18:02:35 +0000

    [extensions]- AmenuManager add hu_HU thanks to sámli
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@4266 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,94 @@
+<?php
+
+$lang['Grum Plugin Classes is not installed'] = 'Szükség van a <b>Grum Plugin Classes</b> bővítményre, telepítenie kell';
+
+$lang['g002_title_page'] = 'Speciális menükezelő';
+$lang['g002_version'] = 'r';
+
+$lang['g002_error_invalid_ajax_call'] = "Érvénytelen függvényhívás !";
+
+$lang['g002_setmenu'] = 'Menü kezelése';
+$lang['g002_addlinks'] = 'Linkek';
+$lang['g002_randompict'] = 'Véletlen kép';
+$lang['g002_personnalblock'] = 'Egyedi menü';
+
+$lang['g002_setmenu_nfo'] = 'Menü elemeinek megjelenítése';
+$lang['g002_addlinks_nfo'] = 'Hyperhivatkozások (hyperlinkek) megjelenítése';
+$lang['g002_randompict_nfo'] = 'Véletlen képek megjelenítése a galériából';
+$lang['g002_personnalblock_nfo'] = 'Menü egyedi tartalmának szerkesztése';
+
+
+$lang['g002_mode_new_window'] = 'Új ablakban';
+$lang['g002_mode_current_window'] = 'Jelenlegi ablakban';
+$lang['g002_addlink'] = 'Link hozzáadása';
+$lang['g002_label'] = 'Cimke';
+$lang['g002_url'] = 'URL';
+$lang['g002_mode'] = 'Megnyitás';
+$lang['g002_icon'] = 'Bemutató kép';
+$lang['g002_visible'] = 'Felsorolás';
+$lang['g002_linkslist'] = "Linkek listája";
+$lang['g002_confirm_delete_link'] = "Link törlése ?";
+$lang['g002_nolinks'] = 'Nincsenek linkek';
+$lang['g002_link'] = 'link';
+$lang['g002_links'] = 'linkek';
+
+$lang['g002_createoflink'] = 'Link hozzáadása';
+$lang['g002_editoflink'] = 'Beállítások módosítása';
+$lang['g002_createthelink'] = 'Link hozzáadása';
+$lang['g002_editthelink'] = 'Link javítása';
+
+$lang['g002_configlinks'] = 'Linkek menüelem beállításai';
+$lang['g002_setting_link_links']='Linkek';
+$lang['g002_setting_link_show_icon'] = 'Linkhez rendelt alapértelmezett kép';
+
+$lang['g002_setting_block_menu'] = 'Menü integráció';
+$lang['g002_setting_block_active'] = 'Blokk megjelenítése a menüben';
+$lang['g002_setting_block_title'] = 'Blokk neve a Menüben';
+$lang['g002_apply'] = 'Küldés';
+
+$lang['g002_sectionslist'] = 'Menü blokkok';
+
+$lang['g002_yesno_y'] = 'Igen';
+$lang['g002_yesno_n'] = 'Nem';
+
+$lang['g002_owner'] = 'Tulajdonos';
+$lang['g002_sectionid'] = 'Azonosító';
+$lang['g002_name'] = 'Cimke';
+
+$lang['g002_translate'] = 'Fordítás a Google Translate használatával';
+
+$lang['g002_configrandompic'] = 'Véletlen kép beállításai';
+$lang['g002_setting_randompic_aboutpicture'] = 'Egyéb jellemzők megjelenítése';
+$lang['g002_setting_randompic_showname'] = 'Kép nevének megjelenítése';
+$lang['g002_setting_randompic_showcomment'] = 'Hozzászólás megjelenítése';
+$lang['g002_show_n'] = 'Nem';
+$lang['g002_show_o'] = 'Megjelenítés a kép felett';
+$lang['g002_show_u'] = 'Megjelenítés a kép alatt';
+
+$lang['g002_personalisedlist'] = "Egyedi blokkok listája";
+$lang['g002_notitle'] = '[Nincs cím]';
+$lang['g002_title'] = 'Cím';
+$lang['g002_sections'] = 'blokkok';
+$lang['g002_section'] = 'blokk';
+$lang['g002_nosections'] = 'Nincsenek blokkok';
+$lang['g002_addsection'] = 'Új blokk';
+$lang['g002_createofpersonalised'] = 'Hozzunk létre egy új személyre szabott blokkot';
+$lang['g002_editofpersonalised'] = 'Blokk módosítása';
+$lang['g002_setting_block_langchoice'] = 'Nyelv';
+$lang['g002_setting_personalised_content'] = 'Blokk tartalma';
+$lang['g002_setting_personalised_properties'] = 'Tulajdonságok';
+$lang['g002_setting_personalised_nfo'] = 'Leírás';
+
+$lang['g002_all_languages'] = "Összes nyelv";
+
+$lang['g002_modmenu'] = '\'Menü\' blokk';
+$lang['g002_modspecial'] = '\'Speciális\' blokk';
+$lang['g002_labelmenu'] = 'Menü cimkék';
+
+$lang['g002_setting_randompic_periodicchange'] = "Periódikus képváltás beállítása";
+$lang['g002_setting_randompic_periodicchange_delay'] = "Képváltás ideje";
+$lang['g002_setting_randompic_periodicchange_deactivated'] = "Kikapcsolva";
+$lang['g002_setting_randompic_height'] = "Menü blokkok szélessége";
+$lang['g002_setting_randompic_height_auto'] = "Automatikus";
+
+?>
