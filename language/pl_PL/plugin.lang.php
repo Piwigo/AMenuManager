4463d2f4e1f3135290be7b2f356b0e0ee44971d5
voyteckst <voyteckst@piwigo.org>
2009-11-17 07:08:04 +0000

    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@4283 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,94 @@
+<?php
+
+$lang['Grum Plugin Classes is not installed'] = 'Plugin <b>Grum Plugin Classes</b> nie jest zainstalowany';
+
+$lang['g002_title_page'] = 'Zaawansowane Zarządzanie Menu';
+$lang['g002_version'] = 'r';
+
+$lang['g002_error_invalid_ajax_call'] = "Niepoprawne wywołanie funkcji !";
+
+$lang['g002_setmenu'] = 'Zarządzanie menu';
+$lang['g002_addlinks'] = 'Linki';
+$lang['g002_randompict'] = 'Losowy obraz';
+$lang['g002_personnalblock'] = 'Menu spersonalizowane';
+
+$lang['g002_setmenu_nfo'] = 'Zarządzanie wyświetlanymi blokami menu';
+$lang['g002_addlinks_nfo'] = 'Blok menu pozwalający na wyświetlanie linków';
+$lang['g002_randompict_nfo'] = 'Blok menu pozwalający na wyświetlanie losowych obrazów z galerii';
+$lang['g002_personnalblock_nfo'] = 'Wyświetlaj spersonalizowane bloki w menu;
+
+
+$lang['g002_mode_new_window'] = 'Nowe okno';
+$lang['g002_mode_current_window'] = 'Aktualne okno';
+$lang['g002_addlink'] = 'Dodaj link';
+$lang['g002_label'] = 'Nazwa';
+$lang['g002_url'] = 'URL';
+$lang['g002_mode'] = 'Tryb';
+$lang['g002_icon'] = 'Obraz reprezentant';
+$lang['g002_visible'] = 'Widoczny';
+$lang['g002_linkslist'] = "Lista linków";
+$lang['g002_confirm_delete_link'] = "Usunąć link ?";
+$lang['g002_nolinks'] = 'Brak linków';
+$lang['g002_link'] = 'link';
+$lang['g002_links'] = 'linki';
+
+$lang['g002_createoflink'] = 'Dodaj link';
+$lang['g002_editoflink'] = 'Edytuj link';
+$lang['g002_createthelink'] = 'Dodaj link';
+$lang['g002_editthelink'] = 'Edytuj link';
+
+$lang['g002_configlinks'] = 'Ustawienia linków';
+$lang['g002_setting_link_links']='Linki';
+$lang['g002_setting_link_show_icon'] = 'Wyświetlaj linki do reprezentujących obrazów';
+
+$lang['g002_setting_block_menu'] = 'Integracja z menu';
+$lang['g002_setting_block_active'] = 'Pokazuj blok w menu';
+$lang['g002_setting_block_title'] = 'Tytuł bloku';
+$lang['g002_apply'] = 'Zastosuj';
+
+$lang['g002_sectionslist'] = 'Bloki menu';
+
+$lang['g002_yesno_y'] = 'Tak';
+$lang['g002_yesno_n'] = 'Nie';
+
+$lang['g002_owner'] = 'Właściciel';
+$lang['g002_sectionid'] = 'Identyfikator';
+$lang['g002_name'] = 'Nazwa';
+
+$lang['g002_translate'] = 'Przetłumacz Translatorem Google';
+
+$lang['g002_configrandompic'] = 'Ustawienia losowych obrazów';
+$lang['g002_setting_randompic_aboutpicture'] = 'Wyświetlane daty';
+$lang['g002_setting_randompic_showname'] = 'Pokazuj nazwy obrazów';
+$lang['g002_setting_randompic_showcomment'] = 'Pokazuj komentarze do obrazów';
+$lang['g002_show_n'] = 'Brak';
+$lang['g002_show_o'] = 'Wyświetlaj nad obrazem';
+$lang['g002_show_u'] = 'Wyświetlaj pod obrazem';
+
+$lang['g002_personalisedlist'] = "Lista spersonalizowanych bloków";
+$lang['g002_notitle'] = '[Brak tytułu]';
+$lang['g002_title'] = 'Tytuł';
+$lang['g002_sections'] = 'bloki';
+$lang['g002_section'] = 'blok';
+$lang['g002_nosections'] = 'Brak';
+$lang['g002_addsection'] = 'dodaj nowy blok';
+$lang['g002_createofpersonalised'] = 'Stwórz nowy spersonalizowany blok';
+$lang['g002_editofpersonalised'] = 'Edytuj spersonalizowany blok';
+$lang['g002_setting_block_langchoice'] = 'Ostatni wybór';
+$lang['g002_setting_personalised_content'] = 'Zawartość bloku';
+$lang['g002_setting_personalised_properties'] = 'Właściwości';
+$lang['g002_setting_personalised_nfo'] = 'Opis';
+
+$lang['g002_all_languages'] = "Wszystkie języki";
+
+$lang['g002_modmenu'] = 'blok menu';
+$lang['g002_modspecial'] = 'blok specjalny';
+$lang['g002_labelmenu'] = 'Nazwa menu';
+
+$lang['g002_setting_randompic_periodicchange'] = "Okresowa zmiana obrazu";
+$lang['g002_setting_randompic_periodicchange_delay'] = "Opóźnienie";
+$lang['g002_setting_randompic_periodicchange_deactivated'] = "Wyłączony";
+$lang['g002_setting_randompic_height'] = "Wysokość bloku menu";
+$lang['g002_setting_randompic_height_auto'] = "Automatycznie";
+
+?>
