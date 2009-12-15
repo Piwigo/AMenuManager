83ddb74a8ae1389618e9a165c34fcc5f53d85425
ddtddt <ddtddt@piwigo.org>
2009-12-15 08:12:39 +0000

    [extensions]- AMM - Add zh_CN thanks to mzs777
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@4496 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,102 @@
+<?php
+
+$lang['Grum Plugin Classes is not installed'] = 'Plugin <b>Grum Plugin Classes</b> in not installed';
+
+$lang['g002_title_page'] = '高级菜单管理';
+$lang['g002_version'] = 'r';
+
+$lang['g002_error_invalid_ajax_call'] = "无效的函数调用";
+
+$lang['g002_setmenu'] = '菜单管理';
+$lang['g002_addlinks'] = '友情连接';
+$lang['g002_randompict'] = '随机图片';
+$lang['g002_personnalblock'] = '个性化菜单';
+
+$lang['g002_setmenu_nfo'] = '菜单显示管理';
+$lang['g002_addlinks_nfo'] = '友情连接管理';
+$lang['g002_randompict_nfo'] = '图库随机图片管理';
+$lang['g002_personnalblock_nfo'] = '添加编辑删除';
+
+
+$lang['g002_mode_new_window'] = '新窗口';
+$lang['g002_mode_current_window'] = '当前窗口';
+$lang['g002_addlink'] = '添加连接';
+$lang['g002_label'] = '网站名称';
+$lang['g002_url'] = '连接地址';
+$lang['g002_mode'] = '模式';
+$lang['g002_icon'] = '代表图片';
+$lang['g002_visible'] = '可见';
+$lang['g002_linkslist'] = "连接列表";
+$lang['g002_confirm_delete_link'] = "确认删除此项";
+$lang['g002_nolinks'] = '没有连接';
+$lang['g002_link'] = '连接';
+$lang['g002_links'] = '连接';
+
+$lang['g002_createoflink'] = '添加连接';
+$lang['g002_editoflink'] = '保存设置';
+$lang['g002_createthelink'] = '添加连接';
+$lang['g002_editthelink'] = '保存设置';
+
+$lang['g002_configlinks'] = '连接设置';
+$lang['g002_setting_link_links']='连接';
+$lang['g002_setting_link_show_icon'] = '是否显示图片的连接地址';
+
+$lang['g002_setting_block_menu'] = '菜单';
+$lang['g002_setting_block_active'] = '是否显示自定义模块';
+$lang['g002_setting_block_title'] = '标题';
+$lang['g002_apply'] = '保存';
+
+$lang['g002_sectionslist'] = '菜单模块';
+
+$lang['g002_yesno_y'] = '是';
+$lang['g002_yesno_n'] = '否';
+
+$lang['g002_owner'] = '所有者';
+$lang['g002_sectionid'] = '标识符';
+$lang['g002_name'] = '标签';
+
+$lang['g002_translate'] = '翻译与谷歌翻译';
+
+$lang['g002_configrandompic'] = '随机图片设置';
+$lang['g002_setting_randompic_aboutpicture'] = '随机图片拥有者评论显示设置';
+$lang['g002_setting_randompic_showname'] = '显示图片拥有者';
+$lang['g002_setting_randompic_showcomment'] = '显示随机图片评论';
+$lang['g002_show_n'] = '否';
+$lang['g002_show_o'] = '在随机图片上显示';
+$lang['g002_show_u'] = '在随机图片下显示';
+
+$lang['g002_personalisedlist'] = "自定义模块";
+$lang['g002_notitle'] = '[无标题]';
+$lang['g002_title'] = '标题';
+$lang['g002_sections'] = '块';
+$lang['g002_section'] = '块';
+$lang['g002_nosections'] = '无自定义模块';
+$lang['g002_addsection'] = '添加新模块';
+$lang['g002_createofpersonalised'] = '创建自定义模块';
+$lang['g002_editofpersonalised'] = '修改自定义模块';
+$lang['g002_setting_block_langchoice'] = '选择';
+$lang['g002_setting_personalised_content'] = '内容';
+$lang['g002_setting_personalised_properties'] = '属性';
+$lang['g002_setting_personalised_nfo'] = '自定义模块名称';
+
+$lang['g002_all_languages'] = "所有语言";
+
+$lang['g002_modmenu'] = '\'menu\' block';
+$lang['g002_modspecial'] = '\'special\' block';
+$lang['g002_labelmenu'] = 'Menu\'s label';
+
+$lang['g002_setting_randompic_periodicchange'] = "定期改变形象";
+$lang['g002_setting_randompic_periodicchange_delay'] = "延迟";
+$lang['g002_setting_randompic_periodicchange_deactivated'] = "关闭";
+$lang['g002_setting_randompic_height'] = "随机图片模块高度";
+$lang['g002_setting_randompic_height_auto'] = "自动";
+
+// v2.1.3
+$lang['g002_cancel'] = "取消";
+$lang['g002_piwigo_default'] = "默认排序";
+$lang['g002_apply_changes'] = "应用更改";
+$lang['g002_config_saved'] = "保存配置";
+$lang['g002_adviser_not_allowed'] = "顾问是不允许进行更改";
+$lang['g002_click_to_manage_rights'] = "设置权限";
+
+?>
