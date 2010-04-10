9b926f6a20c372d866dc2fb683f7c8ca15ab020c
jpr928 <jpr928@mypiwigo.org>
2010-04-10 17:17:35 +0000

    update ES_es language
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@5770 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/es_ES/plugin.lang.php
+++ language/es_ES/plugin.lang.php
@@ -3,46 +3,46 @@
 $lang['To install this plugin, you need to install Grum Plugin Classes %s before'] = 'Para instalar este plugin, usted debe previamente, instalar el plugin Grum Plugin Classes %s';
 
 $lang['g002_title_page'] = 'Gestión avanzada del menú';
-$lang['g002_version'] = 'v';
+$lang['g002_version'] = 'version';
 
-$lang['g002_error_invalid_ajax_call'] = "Llamamiento de función inválida !";
+$lang['g002_error_invalid_ajax_call'] = "Llamada de función inválida !";
 
 $lang['g002_setmenu'] = 'Gestión del menú';
 $lang['g002_addlinks'] = 'Enlaces';
 $lang['g002_randompict'] = 'Imagen aleatoria';
 $lang['g002_personnalblock'] = 'Menú personalizado';
 
-$lang['g002_setmenu_nfo'] = 'Gestión de la fijación de las secciones fijadas en el menú';
-$lang['g002_addlinks_nfo'] = 'Sección que permite fijar en el menú una lista de hipervínculos hacia otros sitios internet';
-$lang['g002_randompict_nfo'] = 'Sección que permite fijar en el menú una imagen tomada al azar en la galería';
-$lang['g002_personnalblock_nfo'] = 'Fijar en el menú de las secciones al contenido personalizado';
+$lang['g002_setmenu_nfo'] = 'Gestión de la visualización de las secciones visualizadas en el menú';
+$lang['g002_addlinks_nfo'] = 'Sección que permite visualizar en el menú una lista de hipervínculos hacia otros sitios internet';
+$lang['g002_randompict_nfo'] = 'Sección que permite visualizar en el menú una imagen tomada al azar en la galería';
+$lang['g002_personnalblock_nfo'] = 'Visualizar en el menú de las secciones al contenido personalizado';
 
 
 $lang['g002_mode_new_window'] = 'Nueva ventana';
-$lang['g002_mode_current_window'] = 'Ventana corriente';
+$lang['g002_mode_current_window'] = 'Ventana actual';
 $lang['g002_addlink'] = 'Añadir un enlace';
-$lang['g002_label'] = 'Redacción';
+$lang['g002_label'] = 'Etiqueta';
 $lang['g002_url'] = 'URL';
 $lang['g002_mode'] = 'Modo';
 $lang['g002_icon'] = 'Imagen';
 $lang['g002_visible'] = 'Visible';
-$lang['g002_linkslist'] = "Lista enlaces";
-$lang['g002_confirm_delete_link'] = "Suprimir el enlace?";
-$lang['g002_nolinks'] = 'Ningunos enlace';
+$lang['g002_linkslist'] = "Lista de enlaces";
+$lang['g002_confirm_delete_link'] = "Eliminar el enlace?";
+$lang['g002_nolinks'] = 'Ningunos enlaces';
 $lang['g002_link'] = 'enlace';
 $lang['g002_links'] = 'enlaces';
 
-$lang['g002_createoflink'] = 'Añadido de un enlace';
+$lang['g002_createoflink'] = 'Añadir un enlace';
 $lang['g002_editoflink'] = 'Modificación de un enlace';
-$lang['g002_createthelink'] = 'Añadido el enlace';
-$lang['g002_editthelink'] = 'Modificación el enlace';
+$lang['g002_createthelink'] = 'Enlace añadido ';
+$lang['g002_editthelink'] = 'Modificación del enlace';
 
 $lang['g002_configlinks'] = 'Configuración de los enlaces';
 $lang['g002_setting_link_links']='Enlaces';
-$lang['g002_setting_link_show_icon'] = 'Fijar los iconos de enlaces';
+$lang['g002_setting_link_show_icon'] = 'Visualizar los iconos de enlaces';
 
 $lang['g002_setting_block_menu'] = 'Integración en el menú';
-$lang['g002_setting_block_active'] = 'Fijar la sección en el menú';
+$lang['g002_setting_block_active'] = 'Visualizar la sección en el menú';
 $lang['g002_setting_block_title'] = 'Título de la sección en el menú';
 $lang['g002_apply'] = 'Aplicar';
 
@@ -52,18 +52,18 @@ $lang['g002_yesno_y'] = 'Sí';
 $lang['g002_yesno_n'] = 'No';
 
 $lang['g002_owner'] = 'Propietario';
-$lang['g002_sectionid'] = 'Identificando';
+$lang['g002_sectionid'] = 'Identificador';
 $lang['g002_name'] = 'Redacción';
 
 $lang['g002_translate'] = 'Traducir con Google Translate';
 
 $lang['g002_configrandompic'] = 'Configuración de la imagen aleatoria';
-$lang['g002_setting_randompic_aboutpicture'] = 'Datos fijados';
-$lang['g002_setting_randompic_showname'] = 'Fijar el nombre de la foto';
-$lang['g002_setting_randompic_showcomment'] = 'Fijar el comentario de la foto';
+$lang['g002_setting_randompic_aboutpicture'] = 'Datos visualizados';
+$lang['g002_setting_randompic_showname'] = 'Visualizar el nombre de la foto';
+$lang['g002_setting_randompic_showcomment'] = 'Visualizar el comentario de la foto';
 $lang['g002_show_n'] = 'No';
-$lang['g002_show_o'] = 'Fijar por encima de la foto';
-$lang['g002_show_u'] = 'Fijar debajo de la foto';
+$lang['g002_show_o'] = 'Visualizar por encima de la foto';
+$lang['g002_show_u'] = 'Visualizar debajo de la foto';
 
 $lang['g002_personalisedlist'] = "Lista secciones personalizadas";
 $lang['g002_notitle'] = '[Ningún título]';
@@ -72,14 +72,14 @@ $lang['g002_sections'] = 'secciones';
 $lang['g002_section'] = 'seccione';
 $lang['g002_nosections'] = 'Ningunas secciones';
 $lang['g002_addsection'] = 'Añadir una sección';
-$lang['g002_createofpersonalised'] = 'Añadido de una sección personalizada';
-$lang['g002_editofpersonalised'] = 'Modificación de una sección personalizada';
-$lang['g002_setting_block_langchoice'] = 'Elección de la idioma';
+$lang['g002_createofpersonalised'] = 'Añadir una sección personalizada';
+$lang['g002_editofpersonalised'] = 'Modificar una sección personalizada';
+$lang['g002_setting_block_langchoice'] = 'Elección del idioma';
 $lang['g002_setting_personalised_content'] = 'Contenido de la sección';
 $lang['g002_setting_personalised_properties'] = 'Propiedades';
 $lang['g002_setting_personalised_nfo'] = 'Descripción';
 
-$lang['g002_all_languages'] = "Todas idiomas";
+$lang['g002_all_languages'] = "Todos los idiomas";
 
 $lang['g002_modspecial'] = 'Sección \'Herramientas\'';
 $lang['g002_modmenu'] = 'Sección \'menú\'';
@@ -95,7 +95,7 @@ $lang['g002_setting_randompic_height_auto'] = "Automático";
 $lang['g002_cancel'] = "Anular";
 $lang['g002_piwigo_default'] = "Selección por defecto de Piwigo";
 $lang['g002_apply_changes'] = "Validar";
-$lang['g002_config_saved'] = "Configuración salvaguardada";
-$lang['g002_adviser_not_allowed'] = "El consejero no es autorizado a efectuar modificaciones";
+$lang['g002_config_saved'] = "Configuración guardada";
+$lang['g002_adviser_not_allowed'] = "El consejero no esta autorizado a efectuar modificaciones";
 $lang['g002_click_to_manage_rights'] = "Administrar los derechos de acceso";
 ?>
