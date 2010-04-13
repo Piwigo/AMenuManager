cfa92f7180024db3f5df48a6b7f1a0f7fe6bb794
keeshessels <keeshessels@piwigo.org>
2010-04-13 14:59:12 +0000

    nl_NL added
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@5833 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,102 @@
+<?php
+
+$lang['Grum Plugin Classes is not installed'] = 'Plugin <b>Grum Plugin Classes</b> in not installed';
+
+$lang['g002_title_page'] = 'Advanced Menu Management';
+$lang['g002_version'] = 'r';
+
+$lang['g002_error_invalid_ajax_call'] = "Ingeldige functie-aanroep !";
+
+$lang['g002_setmenu'] = 'Menu management';
+$lang['g002_addlinks'] = 'Links';
+$lang['g002_randompict'] = 'Willekeurige afbeelding';
+$lang['g002_personnalblock'] = 'Persoonlijk menu';
+
+$lang['g002_setmenu_nfo'] = 'Management van de weergegeven menu-blokken';
+$lang['g002_addlinks_nfo'] = 'Menu-blok waarmee een lijst hyperlinks weergegeven kan worden';
+$lang['g002_randompict_nfo'] = 'Menu-blok waarmee een willekeurige afbeelding van de galerie weergegeven kan worden';
+$lang['g002_personnalblock_nfo'] = 'Maak een eigen menu-blok aan';
+
+
+$lang['g002_mode_new_window'] = 'Nieuw scherm';
+$lang['g002_mode_current_window'] = 'Huidig scherm';
+$lang['g002_addlink'] = 'Voeg een link toe';
+$lang['g002_label'] = 'Label';
+$lang['g002_url'] = 'URL';
+$lang['g002_mode'] = 'Mode';
+$lang['g002_icon'] = 'Representatieve afbeelding';
+$lang['g002_visible'] = 'Zichtbaar';
+$lang['g002_linkslist'] = "Links-lijst";
+$lang['g002_confirm_delete_link'] = "Link verwijderen ?";
+$lang['g002_nolinks'] = 'Geen links';
+$lang['g002_link'] = 'link';
+$lang['g002_links'] = 'links';
+
+$lang['g002_createoflink'] = 'Voeg link toe';
+$lang['g002_editoflink'] = 'Wijzig link';
+$lang['g002_createthelink'] = 'Voeg link toe';
+$lang['g002_editthelink'] = 'Wijzig link';
+
+$lang['g002_configlinks'] = 'Links instellingen';
+$lang['g002_setting_link_links']='Links';
+$lang['g002_setting_link_show_icon'] = 'Geef de representatieve afbeeldingen van de links weer';
+
+$lang['g002_setting_block_menu'] = 'Menu integratie';
+$lang['g002_setting_block_active'] = 'Geef het blok weer in het menu';
+$lang['g002_setting_block_title'] = 'Block titel';
+$lang['g002_apply'] = 'Pas toe';
+
+$lang['g002_sectionslist'] = 'Menu-blok';
+
+$lang['g002_yesno_y'] = 'Ja';
+$lang['g002_yesno_n'] = 'Nee';
+
+$lang['g002_owner'] = 'Eigenaar';
+$lang['g002_sectionid'] = 'Identifier';
+$lang['g002_name'] = 'Label';
+
+$lang['g002_translate'] = 'Vertaal met Google Vertalen';
+
+$lang['g002_configrandompic'] = 'Instellingen van de willekeurige afbeelding';
+$lang['g002_setting_randompic_aboutpicture'] = 'Weergegeven gegevens';
+$lang['g002_setting_randompic_showname'] = 'Geef de naam van de afbeelding weer';
+$lang['g002_setting_randompic_showcomment'] = 'Laat de commentaren bij de afbeelding zien';
+$lang['g002_show_n'] = 'Nee';
+$lang['g002_show_o'] = 'Geef weer over de afbeelding';
+$lang['g002_show_u'] = 'Geef weer onder de afbeelding';
+
+$lang['g002_personalisedlist'] = "Lijst met gepersonaliseerde blokken";
+$lang['g002_notitle'] = '[Geen titel]';
+$lang['g002_title'] = 'Titel';
+$lang['g002_sections'] = 'blokken';
+$lang['g002_section'] = 'blok';
+$lang['g002_nosections'] = 'Geen blokken';
+$lang['g002_addsection'] = 'Voeg een nieuw blok toe';
+$lang['g002_createofpersonalised'] = 'Maak een nieuw gersonaliseerd blok aan';
+$lang['g002_editofpersonalised'] = 'Wijzig een gepersonaliseerd blok';
+$lang['g002_setting_block_langchoice'] = 'Taal';
+$lang['g002_setting_personalised_content'] = 'Inhoud';
+$lang['g002_setting_personalised_properties'] = 'Eigenschappen';
+$lang['g002_setting_personalised_nfo'] = 'Beschrijving';
+
+$lang['g002_all_languages'] = "Alle talen";
+
+$lang['g002_modmenu'] = '\'menu\' blok';
+$lang['g002_modspecial'] = '\'special\' blok';
+$lang['g002_labelmenu'] = 'Menu label';
+
+$lang['g002_setting_randompic_periodicchange'] = "Periodieke wijziging van de afbeelding";
+$lang['g002_setting_randompic_periodicchange_delay'] = "Vertraging";
+$lang['g002_setting_randompic_periodicchange_deactivated'] = "Niet in gebruik";
+$lang['g002_setting_randompic_height'] = "Hoogte van het menu-blok";
+$lang['g002_setting_randompic_height_auto'] = "Automatisch";
+
+// v2.1.3
+$lang['g002_cancel'] = "Annuleren";
+$lang['g002_piwigo_default'] = "Piwigo\'s standaard sortering";
+$lang['g002_apply_changes'] = "Pas de wijzigingen toe";
+$lang['g002_config_saved'] = "De configuratie is opgeslagen";
+$lang['g002_adviser_not_allowed'] = "Het is een Adviser niet toegestaan om wijzigingen te amken";
+$lang['g002_click_to_manage_rights'] = "Rechten management";
+
+?>
