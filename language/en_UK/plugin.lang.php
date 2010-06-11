1b56b8531f9e6d2b8bb5ba84fd874b6be4e0430d
tosca <tosca@mypiwigo.org>
2010-06-11 22:08:37 +0000

    Fix some english typos and mistakes
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@6521 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/en_UK/plugin.lang.php
+++ language/en_UK/plugin.lang.php
@@ -46,7 +46,7 @@ $lang['g002_setting_block_active'] = 'Display block in menu';
 $lang['g002_setting_block_title'] = 'Block title';
 $lang['g002_apply'] = 'Apply';
 
-$lang['g002_sectionslist'] = 'Menu\'s blocks';
+$lang['g002_sectionslist'] = 'Menu blocks';
 
 $lang['g002_yesno_y'] = 'Yes';
 $lang['g002_yesno_n'] = 'No';
@@ -59,13 +59,13 @@ $lang['g002_translate'] = 'Translate with Google Translate';
 
 $lang['g002_configrandompic'] = 'Random picture settings';
 $lang['g002_setting_randompic_aboutpicture'] = 'Displayed datas';
-$lang['g002_setting_randompic_showname'] = 'Show picture\'s name';
-$lang['g002_setting_randompic_showcomment'] = 'Show picure\'s comment';
+$lang['g002_setting_randompic_showname'] = 'Show picture name';
+$lang['g002_setting_randompic_showcomment'] = 'Show picture comment';
 $lang['g002_show_n'] = 'No';
-$lang['g002_show_o'] = 'Display over the picture';
-$lang['g002_show_u'] = 'Display under thpicture';
+$lang['g002_show_o'] = 'Display above the picture';
+$lang['g002_show_u'] = 'Display under the picture';
 
-$lang['g002_personalisedlist'] = "Liste of personalised blocks";
+$lang['g002_personalisedlist'] = "List of personalized blocks";
 $lang['g002_notitle'] = '[No title]';
 $lang['g002_title'] = 'Title';
 $lang['g002_sections'] = 'blocks';
@@ -74,8 +74,8 @@ $lang['g002_nosections'] = 'No blocks';
 $lang['g002_addsection'] = 'Add a new block';
 $lang['g002_createofpersonalised'] = 'Create a new personalized block';
 $lang['g002_editofpersonalised'] = 'Modify a personalized block';
-$lang['g002_setting_block_langchoice'] = 'Lang choice';
-$lang['g002_setting_personalised_content'] = 'Block\'s content';
+$lang['g002_setting_block_langchoice'] = 'Language choice';
+$lang['g002_setting_personalised_content'] = 'Block content';
 $lang['g002_setting_personalised_properties'] = 'Properties';
 $lang['g002_setting_personalised_nfo'] = 'Description';
 
@@ -83,7 +83,7 @@ $lang['g002_all_languages'] = "All languages";
 
 $lang['g002_modmenu'] = '\'menu\' block';
 $lang['g002_modspecial'] = '\'special\' block';
-$lang['g002_labelmenu'] = 'Menu\'s label';
+$lang['g002_labelmenu'] = 'Menu label';
 
 $lang['g002_setting_randompic_periodicchange'] = "Periodic change of the image";
 $lang['g002_setting_randompic_periodicchange_delay'] = "Delay";
