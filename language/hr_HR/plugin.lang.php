90fddb877e5cd656a356fc599aa673f26c3cdd95
ddtddt <ddtddt@piwigo.org>
2010-08-07 12:02:05 +0000

    [extension] AMenuManager, Add hr_HR thanks to LuxZg
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@6746 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hr_HR/plugin.lang.php
@@ -0,0 +1,102 @@
+<?php
+
+$lang['Grum Plugin Classes is not installed'] = 'Plugin <b>Grum Plugin Classes</b> nije instaliran';
+
+$lang['g002_title_page'] = 'Advanced Menu Management';
+$lang['g002_version'] = 'r';
+
+$lang['g002_error_invalid_ajax_call'] = "Nevažeći poziv funkcije !";
+
+$lang['g002_setmenu'] = 'Upravljanje izbornicima';
+$lang['g002_addlinks'] = 'Linkovi';
+$lang['g002_randompict'] = 'Slučajno odabrana slika';
+$lang['g002_personnalblock'] = 'Personalizirani izbornik';
+
+$lang['g002_setmenu_nfo'] = 'Upravljanje prikazanih blokova izbornika';
+$lang['g002_addlinks_nfo'] = 'Blok izbornika koji omogućuje prikaz liste hiperlinkova';
+$lang['g002_randompict_nfo'] = 'Blok izbornika koji omogućuje prikaz slučajno odabrane slike iz galerije';
+$lang['g002_personnalblock_nfo'] = 'Prikaz personaliziranog bloka u izborniku';
+
+
+$lang['g002_mode_new_window'] = 'Novi prozor';
+$lang['g002_mode_current_window'] = 'Trenutni prozor';
+$lang['g002_addlink'] = 'Dodaj link';
+$lang['g002_label'] = 'Naziv';
+$lang['g002_url'] = 'URL';
+$lang['g002_mode'] = 'Mod';
+$lang['g002_icon'] = 'Slika koja predstavlja link';
+$lang['g002_visible'] = 'Vidljiv';
+$lang['g002_linkslist'] = "Lista linkova";
+$lang['g002_confirm_delete_link'] = "Obrisati link ?";
+$lang['g002_nolinks'] = 'Bez linkova';
+$lang['g002_link'] = 'link';
+$lang['g002_links'] = 'linkovi';
+
+$lang['g002_createoflink'] = 'Dodaj link';
+$lang['g002_editoflink'] = 'Izmjeni link';
+$lang['g002_createthelink'] = 'Dodaj link';
+$lang['g002_editthelink'] = 'Izmjeni link';
+
+$lang['g002_configlinks'] = 'Postavke linka';
+$lang['g002_setting_link_links']='Linkovi';
+$lang['g002_setting_link_show_icon'] = 'Prikaz slike koja predstavlja link';
+
+$lang['g002_setting_block_menu'] = 'Integracija izbornika';
+$lang['g002_setting_block_active'] = 'Prikaz bloka u izborniku';
+$lang['g002_setting_block_title'] = 'Naslov bloka';
+$lang['g002_apply'] = 'Primjeni';
+
+$lang['g002_sectionslist'] = 'Blokovi izbornika';
+
+$lang['g002_yesno_y'] = 'Da';
+$lang['g002_yesno_n'] = 'Ne';
+
+$lang['g002_owner'] = 'Vlasnik';
+$lang['g002_sectionid'] = 'Identifikator';
+$lang['g002_name'] = 'Naziv';
+
+$lang['g002_translate'] = 'Prevedi sa Google Translate';
+
+$lang['g002_configrandompic'] = 'Postavke slučajno odabrane slike';
+$lang['g002_setting_randompic_aboutpicture'] = 'Prikazani podaci';
+$lang['g002_setting_randompic_showname'] = 'Prikaži naziv slike';
+$lang['g002_setting_randompic_showcomment'] = 'Prikaži komentare slike';
+$lang['g002_show_n'] = 'Ne';
+$lang['g002_show_o'] = 'Prikaži iznad slike';
+$lang['g002_show_u'] = 'Prikaži ispod slike';
+
+$lang['g002_personalisedlist'] = "Lista personaliziranih blokova";
+$lang['g002_notitle'] = '[Bez naslova]';
+$lang['g002_title'] = 'Naslov';
+$lang['g002_sections'] = 'blokovi';
+$lang['g002_section'] = 'blok';
+$lang['g002_nosections'] = 'Bez blokova';
+$lang['g002_addsection'] = 'Dodaj novi blok';
+$lang['g002_createofpersonalised'] = 'Kreiraj novi personalizirani blok';
+$lang['g002_editofpersonalised'] = 'Izmjeni personalizirani blok';
+$lang['g002_setting_block_langchoice'] = 'Izbor jezika';
+$lang['g002_setting_personalised_content'] = 'Sadržaj bloka';
+$lang['g002_setting_personalised_properties'] = 'Svojstva';
+$lang['g002_setting_personalised_nfo'] = 'Opis';
+
+$lang['g002_all_languages'] = "Svi jezici";
+
+$lang['g002_modmenu'] = '\'menu\' block';
+$lang['g002_modspecial'] = '\'special\' block';
+$lang['g002_labelmenu'] = 'Naslov izbornika';
+
+$lang['g002_setting_randompic_periodicchange'] = "Periodična promjena slike";
+$lang['g002_setting_randompic_periodicchange_delay'] = "Zadrška";
+$lang['g002_setting_randompic_periodicchange_deactivated'] = "Onemogućeno";
+$lang['g002_setting_randompic_height'] = "Visina bloka";
+$lang['g002_setting_randompic_height_auto'] = "Automatski";
+
+// v2.1.3
+$lang['g002_cancel'] = "Otkaži";
+$lang['g002_piwigo_default'] = "Zadani Piwigo poredak";
+$lang['g002_apply_changes'] = "Primjeni izmjene";
+$lang['g002_config_saved'] = "Postavke su spremljene";
+$lang['g002_adviser_not_allowed'] = "Savjetniku nije dozvoljeno raditi izmjene";
+$lang['g002_click_to_manage_rights'] = "Upravljanje pravima";
+
+?>
