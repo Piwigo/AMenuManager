afe70a89cc89c18cb7984ea4cf5532f6b41af56c
ddtddt <ddtddt@piwigo.org>
2010-10-18 20:42:47 +0000

    [extension] AMenuManager add cs_CZ thanks to webprostor
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@7271 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,102 @@
+<?php
+
+$lang['Grum Plugin Classes is not installed'] = 'Plugin <b>Grum Plugin Classes</b> není nainstalovaný';
+
+$lang['g002_title_page'] = 'Advanced Menu Management';
+$lang['g002_version'] = 'r';
+
+$lang['g002_error_invalid_ajax_call'] = "Invalid function call !";
+
+$lang['g002_setmenu'] = 'Menu management';
+$lang['g002_addlinks'] = 'Linky';
+$lang['g002_randompict'] = 'Nahodný obrázek';
+$lang['g002_personnalblock'] = 'Personalizované menu';
+
+$lang['g002_setmenu_nfo'] = 'Management pro zobrazení bloků menu';
+$lang['g002_addlinks_nfo'] = 'Menu blok povolující zobrazení seznamu hyperlinků';
+$lang['g002_randompict_nfo'] = 'Menu blok povolující zobrazení nahodného obrázku z galerie';
+$lang['g002_personnalblock_nfo'] = 'Zobrazí personalisované bloky v menu';
+
+
+$lang['g002_mode_new_window'] = 'Nové okno';
+$lang['g002_mode_current_window'] = 'Stávající okno';
+$lang['g002_addlink'] = 'Přidat link';
+$lang['g002_label'] = 'Popis';
+$lang['g002_url'] = 'URL';
+$lang['g002_mode'] = 'Mode';
+$lang['g002_icon'] = 'Reprezentující obrázek';
+$lang['g002_visible'] = 'Viditelné';
+$lang['g002_linkslist'] = "Seznam odkazů";
+$lang['g002_confirm_delete_link'] = "Smazat odkaz ?";
+$lang['g002_nolinks'] = 'Žádné odkazy';
+$lang['g002_link'] = 'odkaz';
+$lang['g002_links'] = 'odkazy';
+
+$lang['g002_createoflink'] = 'Přidat link';
+$lang['g002_editoflink'] = 'Edituj link';
+$lang['g002_createthelink'] = 'Přidat link';
+$lang['g002_editthelink'] = 'Editovat link';
+
+$lang['g002_configlinks'] = 'Nastavení odkazů';
+$lang['g002_setting_link_links']='Linky';
+$lang['g002_setting_link_show_icon'] = 'Zobraz u odkazů reprezentující obrázek';
+
+$lang['g002_setting_block_menu'] = 'Integrace menu';
+$lang['g002_setting_block_active'] = 'Zobraz blok v menu';
+$lang['g002_setting_block_title'] = 'Blok nadpis';
+$lang['g002_apply'] = 'Potvrď';
+
+$lang['g002_sectionslist'] = 'Menu bloky';
+
+$lang['g002_yesno_y'] = 'Ano';
+$lang['g002_yesno_n'] = 'Ne';
+
+$lang['g002_owner'] = 'Vlastník';
+$lang['g002_sectionid'] = 'Identifikátor';
+$lang['g002_name'] = 'Popisek';
+
+$lang['g002_translate'] = 'Přeložit pomocí Google Translate';
+
+$lang['g002_configrandompic'] = 'Náhodný obrázek nastaevní';
+$lang['g002_setting_randompic_aboutpicture'] = 'Zobrazená data';
+$lang['g002_setting_randompic_showname'] = 'Ukaž název obrázku';
+$lang['g002_setting_randompic_showcomment'] = 'Ukaž komentář obrázku';
+$lang['g002_show_n'] = 'Ne';
+$lang['g002_show_o'] = 'Zobrazení nad obrázkem';
+$lang['g002_show_u'] = 'Zobrazení pod obrázkem';
+
+$lang['g002_personalisedlist'] = "Seznam personalizovaných bloků";
+$lang['g002_notitle'] = '[Žádný nadpis]';
+$lang['g002_title'] = 'Nadpis';
+$lang['g002_sections'] = 'bloky';
+$lang['g002_section'] = 'blok';
+$lang['g002_nosections'] = 'Žádné bloky';
+$lang['g002_addsection'] = 'Přidat nový blok';
+$lang['g002_createofpersonalised'] = 'Vytvořit nový personalizovaný blok';
+$lang['g002_editofpersonalised'] = 'Modifikuj personalizovaný blok';
+$lang['g002_setting_block_langchoice'] = 'Vyber jazyk';
+$lang['g002_setting_personalised_content'] = 'Obsah bloku';
+$lang['g002_setting_personalised_properties'] = 'Vlastnosti';
+$lang['g002_setting_personalised_nfo'] = 'Popis';
+
+$lang['g002_all_languages'] = "Všechny jazyky";
+
+$lang['g002_modmenu'] = '\'menu\' blok';
+$lang['g002_modspecial'] = '\'special\' blok';
+$lang['g002_labelmenu'] = 'Menu název';
+
+$lang['g002_setting_randompic_periodicchange'] = "Periodická změna obrázku";
+$lang['g002_setting_randompic_periodicchange_delay'] = "Zpoždění";
+$lang['g002_setting_randompic_periodicchange_deactivated'] = "Vypnuto";
+$lang['g002_setting_randompic_height'] = "Výška bloků menu";
+$lang['g002_setting_randompic_height_auto'] = "Automat";
+
+// v2.1.3
+$lang['g002_cancel'] = "Zrušit";
+$lang['g002_piwigo_default'] = "Piwigo defaultní řazení";
+$lang['g002_apply_changes'] = "Aplikuj změny";
+$lang['g002_config_saved'] = "Konfigurace uložena";
+$lang['g002_adviser_not_allowed'] = "Není Vám dovoleno provádět změny";
+$lang['g002_click_to_manage_rights'] = "Práva managementu";
+
+?>
