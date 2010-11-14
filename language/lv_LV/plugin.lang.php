546132fc3ad76c04878a2d6196e8a2f0f5b52785
ddtddt <ddtddt@piwigo.org>
2010-11-14 20:18:06 +0000

    [extensions] - AMenuManager -  add lv_LV (Latvian) thanks to Aivars Baldone
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@7777 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,102 @@
+<?php
+
+$lang['Grum Plugin Classes is not installed'] = 'Spraudnis <b>Grum Plugin Classes</b> nav uzinstalēts';
+
+$lang['g002_title_page'] = 'Izvērstā Izvēlnes Pārvaldīšana';
+$lang['g002_version'] = 'r';
+
+$lang['g002_error_invalid_ajax_call'] = "Kļūdains funkcijas izsaukums !";
+
+$lang['g002_setmenu'] = 'Izvēlnes pārvaldība';
+$lang['g002_addlinks'] = 'Saites';
+$lang['g002_randompict'] = 'Gadījuma attēls';
+$lang['g002_personnalblock'] = 'Personificēta izvēlne';
+
+$lang['g002_setmenu_nfo'] = 'Attēlojamo izvēlnes/ņu bloku pārvaldība ';
+$lang['g002_addlinks_nfo'] = 'Izvēlnes/ņu bloks, kas ļauj parādīt hipersaišu sarakstu';
+$lang['g002_randompict_nfo'] = 'Izvēlnes/ņu bloks, kas ļauj parādīt gadījuma attēlu no galerijas';
+$lang['g002_personnalblock_nfo'] = 'Parādīt personificētus (pielāgotus) blokus izvēlnē';
+
+
+$lang['g002_mode_new_window'] = 'Jauns logs';
+$lang['g002_mode_current_window'] = 'Patrreizējais logs';
+$lang['g002_addlink'] = 'Pievienot saiti';
+$lang['g002_label'] = 'Uzraksts';
+$lang['g002_url'] = 'URL';
+$lang['g002_mode'] = 'Režīms';
+$lang['g002_icon'] = 'Pārstāvētājattēls';
+$lang['g002_visible'] = 'Redzams';
+$lang['g002_linkslist'] = "Saišu saraksts";
+$lang['g002_confirm_delete_link'] = "Izdzēst saiti ?";
+$lang['g002_nolinks'] = 'Nav saišu';
+$lang['g002_link'] = 'saite';
+$lang['g002_links'] = 'saites';
+
+$lang['g002_createoflink'] = 'Pievienot saiti';
+$lang['g002_editoflink'] = 'Rediģēt saiti';
+$lang['g002_createthelink'] = 'Pievienot saiti';
+$lang['g002_editthelink'] = 'Rediģēt saiti';
+
+$lang['g002_configlinks'] = 'Saišu iestatījumi';
+$lang['g002_setting_link_links']='Saites';
+$lang['g002_setting_link_show_icon'] = 'Parādīt saiti pārstāvošo attēlu';
+
+$lang['g002_setting_block_menu'] = 'Izvēlnes integrācija (apvienošana)';
+$lang['g002_setting_block_active'] = 'Parādīt bloku izvēknē';
+$lang['g002_setting_block_title'] = 'Bloka nosaukums';
+$lang['g002_apply'] = 'Pielietot';
+
+$lang['g002_sectionslist'] = 'Izvēlņu bloki';
+
+$lang['g002_yesno_y'] = 'Jā';
+$lang['g002_yesno_n'] = 'Nē';
+
+$lang['g002_owner'] = 'Saimnieks';
+$lang['g002_sectionid'] = 'Identifikators';
+$lang['g002_name'] = 'Uzraksts';
+
+$lang['g002_translate'] = 'Tulkot ar Google Translate';
+
+$lang['g002_configrandompic'] = 'Gadījuma attēla iestatījumi';
+$lang['g002_setting_randompic_aboutpicture'] = 'Attēlojamie dati';
+$lang['g002_setting_randompic_showname'] = 'Parādīt attēla nosaukumu';
+$lang['g002_setting_randompic_showcomment'] = 'Parādīt attēla komentāru';
+$lang['g002_show_n'] = 'Nē';
+$lang['g002_show_o'] = 'Parādīt virs attēla';
+$lang['g002_show_u'] = 'Parādīt zem attēla';
+
+$lang['g002_personalisedlist'] = "Pielāgoto bloku saraksts";
+$lang['g002_notitle'] = '[No title]';
+$lang['g002_title'] = 'Virsraksts';
+$lang['g002_sections'] = 'bloki';
+$lang['g002_section'] = 'bloks';
+$lang['g002_nosections'] = 'Nav bloku';
+$lang['g002_addsection'] = 'Pievienienot jaunu bloku';
+$lang['g002_createofpersonalised'] = 'Izveidot jaunu pielāgotu bloku';
+$lang['g002_editofpersonalised'] = 'Modificēt pielāgoto bloku';
+$lang['g002_setting_block_langchoice'] = 'Valodas izvēle';
+$lang['g002_setting_personalised_content'] = 'Bloka saturs';
+$lang['g002_setting_personalised_properties'] = 'Īpašības';
+$lang['g002_setting_personalised_nfo'] = 'Apraksts';
+
+$lang['g002_all_languages'] = "Visas valodas";
+
+$lang['g002_modmenu'] = '\'izvēlne\' bloks';
+$lang['g002_modspecial'] = '\'speciāls\' bloks';
+$lang['g002_labelmenu'] = 'Izvēlnes uzraksts';
+
+$lang['g002_setting_randompic_periodicchange'] = "Attēla izmaiņu periodiskums";
+$lang['g002_setting_randompic_periodicchange_delay'] = "Kavējums";
+$lang['g002_setting_randompic_periodicchange_deactivated'] = "Atspējots";
+$lang['g002_setting_randompic_height'] = "Izvēlnes bloka augstums";
+$lang['g002_setting_randompic_height_auto'] = "Automātiski";
+
+// v2.1.3
+$lang['g002_cancel'] = "Atcelt";
+$lang['g002_piwigo_default'] = "Piwigo kārtojums pēc noklusēšanas";
+$lang['g002_apply_changes'] = "Apstiprināt izmaiņas";
+$lang['g002_config_saved'] = "Konfigurācija ir saglabāta";
+$lang['g002_adviser_not_allowed'] = "Padomdevējam nav tiesību izdarīt izmaiņas";
+$lang['g002_click_to_manage_rights'] = "Tiesību pārvaldīšana";
+
+?>
