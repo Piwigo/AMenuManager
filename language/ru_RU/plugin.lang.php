9ffb8ad900fa464c7ab218b92fec0ca3f3e22dc1
ddtddt <ddtddt@piwigo.org>
2010-11-22 19:00:38 +0000

    [Branche 2.1] - AMenuManager - Add ru_RU thanks to nadusha
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@7837 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,102 @@
+<?php
+
+$lang['Grum Plugin Classes is not installed'] = 'Плагин <b>Grum Plugin Classes</b> не установлен'; 
+
+$lang['g002_title_page'] = 'Advanced Menu Management'; 
+$lang['g002_version'] = 'r'; 
+
+$lang['g002_error_invalid_ajax_call'] = "Ошибка вызова функции!"; 
+
+$lang['g002_setmenu'] = 'Настройка Меню'; 
+$lang['g002_addlinks'] = 'Ссылки'; 
+$lang['g002_randompict'] = 'Случайная картинка'; 
+$lang['g002_personnalblock'] = 'Индивидуальное меню'; 
+
+$lang['g002_setmenu_nfo'] = 'Настройка отображения блоков меню'; 
+$lang['g002_addlinks_nfo'] = 'Блок меню позволяет отобразить список ссылок'; 
+$lang['g002_randompict_nfo'] = 'Блок меню позволяет отобразить случайную картинку из галереи'; 
+$lang['g002_personnalblock_nfo'] = 'Отображает персонализированный блок в меню'; 
+
+
+$lang['g002_mode_new_window'] = 'в новом окне'; 
+$lang['g002_mode_current_window'] = 'в текущем окне'; 
+$lang['g002_addlink'] = 'Добавить ссылку'; 
+$lang['g002_label'] = 'Надпись'; 
+$lang['g002_url'] = 'URL'; 
+$lang['g002_mode'] = 'Открывать ссылку'; 
+$lang['g002_icon'] = 'Отображаемая картинка'; 
+$lang['g002_visible'] = 'Показать'; 
+$lang['g002_linkslist'] = "Список ссылок"; 
+$lang['g002_confirm_delete_link'] = "Удалить ссылку?"; 
+$lang['g002_nolinks'] = 'Нет ссылок'; 
+$lang['g002_link'] = 'ссылка'; 
+$lang['g002_links'] = 'ссылки'; 
+
+$lang['g002_createoflink'] = 'Добавить ссылку'; 
+$lang['g002_editoflink'] = 'Редактировать ссылку'; 
+$lang['g002_createthelink'] = 'Добавить ссылку'; 
+$lang['g002_editthelink'] = 'Редактировать ссылку'; 
+
+$lang['g002_configlinks'] = 'Настройка ссылок'; 
+$lang['g002_setting_link_links']='Ссылки'; 
+$lang['g002_setting_link_show_icon'] = 'Показать отображаемую картинку ссылок'; 
+
+$lang['g002_setting_block_menu'] = 'Объединение меню'; 
+$lang['g002_setting_block_active'] = 'Показать блок в меню'; 
+$lang['g002_setting_block_title'] = 'Заголовок блока'; 
+$lang['g002_apply'] = 'Применить'; 
+
+$lang['g002_sectionslist'] = 'Блоки меню'; 
+
+$lang['g002_yesno_y'] = 'Да'; 
+$lang['g002_yesno_n'] = 'Нет'; 
+
+$lang['g002_owner'] = 'Владелец'; 
+$lang['g002_sectionid'] = 'Идентификатор'; 
+$lang['g002_name'] = 'Надпись'; 
+
+$lang['g002_translate'] = 'Перевести с помощью Google Translate'; 
+
+$lang['g002_configrandompic'] = 'Настройка случайной картинки'; 
+$lang['g002_setting_randompic_aboutpicture'] = 'Отображаемые даты'; 
+$lang['g002_setting_randompic_showname'] = 'Показать название картинки'; 
+$lang['g002_setting_randompic_showcomment'] = 'Показать комментарии к картинке'; 
+$lang['g002_show_n'] = 'Нет'; 
+$lang['g002_show_o'] = 'Отобразить над картинкой'; 
+$lang['g002_show_u'] = 'Отобразить под картинкой'; 
+
+$lang['g002_personalisedlist'] = "Список индивидуальных блоков"; 
+$lang['g002_notitle'] = '[Без названия]'; 
+$lang['g002_title'] = 'Название'; 
+$lang['g002_sections'] = 'блоки'; 
+$lang['g002_section'] = 'блок'; 
+$lang['g002_nosections'] = 'Нет блоков'; 
+$lang['g002_addsection'] = 'Добавить новый блок'; 
+$lang['g002_createofpersonalised'] = 'Создать новый индивидуальный блок'; 
+$lang['g002_editofpersonalised'] = 'Изменить индивидуальный блок'; 
+$lang['g002_setting_block_langchoice'] = 'Выбрать язык'; 
+$lang['g002_setting_personalised_content'] = 'Содержание блока'; 
+$lang['g002_setting_personalised_properties'] = 'Свойства'; 
+$lang['g002_setting_personalised_nfo'] = 'Описание'; 
+
+$lang['g002_all_languages'] = "Все языки"; 
+
+$lang['g002_modmenu'] = 'блок \'меню\''; 
+$lang['g002_modspecial'] = 'блок \'специальный\''; 
+$lang['g002_labelmenu'] = 'Ярлык меню'; 
+
+$lang['g002_setting_randompic_periodicchange'] = "Периодичность смены изображений"; 
+$lang['g002_setting_randompic_periodicchange_delay'] = "Задержка"; 
+$lang['g002_setting_randompic_periodicchange_deactivated'] = "Невозможно"; 
+$lang['g002_setting_randompic_height'] = "Высота блока меню"; 
+$lang['g002_setting_randompic_height_auto'] = "Автоматически"; 
+
+// v2.1.3 
+$lang['g002_cancel'] = "Отмена"; 
+$lang['g002_piwigo_default'] = "Сортировка по умолчанию Piwigo"; 
+$lang['g002_apply_changes'] = "Применить изменения"; 
+$lang['g002_config_saved'] = "Конфигурация сохранена"; 
+$lang['g002_adviser_not_allowed'] = "Консультант не уполномочен совершать изменения";
+$lang['g002_click_to_manage_rights'] = "Управление правами";
+
+?>
