353475833f50a0811c31eb75781cba59ad54aa93
ddtddt <ddtddt@piwigo.org>
2011-01-16 21:14:22 +0000

    [extensions] - AMenuManager - add ar_SA thanks to bakir
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@8714 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ar_SA/plugin.lang.php
@@ -0,0 +1,102 @@
+<?php
+
+$lang['Grum Plugin Classes is not installed'] = 'الاداة <b>Grum Plugin Classes</b> غير مثبتة';
+
+$lang['g002_title_page'] = 'تنظيم القوائم الموسع';
+$lang['g002_version'] = 'اصدار';
+
+$lang['g002_error_invalid_ajax_call'] = "طريقة خاطئة في استدعاء الوظيفة";
+
+$lang['g002_setmenu'] = 'تنظيم القائمة';
+$lang['g002_addlinks'] = 'روابط';
+$lang['g002_randompict'] = 'صورة عشوائية';
+$lang['g002_personnalblock'] = 'قائمة مخصصة';
+
+$lang['g002_setmenu_nfo'] = 'تنظيم المناطق المعروضة';
+$lang['g002_addlinks_nfo'] = 'منطقة القوائم تسمح بعرض مجموعة من الروابط';
+$lang['g002_randompict_nfo'] = 'منطقة القوائم تسمح بعرض صورة عشوائية من المعرض';
+$lang['g002_personnalblock_nfo'] = 'عرض المناطق المخصصة في القائمة';
+
+
+$lang['g002_mode_new_window'] = 'نافذة جديدة';
+$lang['g002_mode_current_window'] = 'النافذة الحالية';
+$lang['g002_addlink'] = 'اضافة رابط';
+$lang['g002_label'] = 'ملصق';
+$lang['g002_url'] = 'رابط';
+$lang['g002_mode'] = 'وضع';
+$lang['g002_icon'] = 'الصورة الممثلة';
+$lang['g002_visible'] = 'مرئي';
+$lang['g002_linkslist'] = "قائمة روابط";
+$lang['g002_confirm_delete_link'] = "حذف الرابط";
+$lang['g002_nolinks'] = 'لا روابط';
+$lang['g002_link'] = 'رابط';
+$lang['g002_links'] = 'روابط';
+
+$lang['g002_createoflink'] = 'اضافة رابط';
+$lang['g002_editoflink'] = 'تعديل رابط';
+$lang['g002_createthelink'] = 'اضافة رابط';
+$lang['g002_editthelink'] = 'تعديل رابط';
+
+$lang['g002_configlinks'] = 'اعدادات الروابط';
+$lang['g002_setting_link_links']='روابط';
+$lang['g002_setting_link_show_icon'] = 'عرض الصورة الممثلة للروابط';
+
+$lang['g002_setting_block_menu'] = 'دمج مع القائمة';
+$lang['g002_setting_block_active'] = 'عرض المنطقة في القائمة';
+$lang['g002_setting_block_title'] = 'عنوان المنطقة';
+$lang['g002_apply'] = 'تطبيق';
+
+$lang['g002_sectionslist'] = 'مناطق قائمة';
+
+$lang['g002_yesno_y'] = 'نعم';
+$lang['g002_yesno_n'] = 'لا';
+
+$lang['g002_owner'] = 'مالك';
+$lang['g002_sectionid'] = 'المعرف';
+$lang['g002_name'] = 'لاصق';
+
+$lang['g002_translate'] = 'ترجم بواسطة جوجل';
+
+$lang['g002_configrandompic'] = 'اعدادات الصورة العشوائية';
+$lang['g002_setting_randompic_aboutpicture'] = 'المعلومات المعروضة';
+$lang['g002_setting_randompic_showname'] = 'اظهر اسم الصورة';
+$lang['g002_setting_randompic_showcomment'] = 'اظهر تعليق الصورة';
+$lang['g002_show_n'] = 'لا';
+$lang['g002_show_o'] = 'اظهر اعلى الصورة';
+$lang['g002_show_u'] = 'اظهر اسفل الصورة';
+
+$lang['g002_personalisedlist'] = "قائمة بالمناطق المخصصة";
+$lang['g002_notitle'] = 'لا عنوان';
+$lang['g002_title'] = 'العنوان';
+$lang['g002_sections'] = 'مناطق';
+$lang['g002_section'] = 'منطقة';
+$lang['g002_nosections'] = 'لا مناطق';
+$lang['g002_addsection'] = 'اضافة منطقة جديدة';
+$lang['g002_createofpersonalised'] = 'خلق منطقة جديدة مخصصة';
+$lang['g002_editofpersonalised'] = 'تعديل منطقة مخصصة';
+$lang['g002_setting_block_langchoice'] = 'اختيار اللغة';
+$lang['g002_setting_personalised_content'] = 'محتوي المنطقة';
+$lang['g002_setting_personalised_properties'] = 'خصائص';
+$lang['g002_setting_personalised_nfo'] = 'وصف';
+
+$lang['g002_all_languages'] = "كل اللغات";
+
+$lang['g002_modmenu'] = 'منطقة القائمة';
+$lang['g002_modspecial'] = 'منطقة خاصة';
+$lang['g002_labelmenu'] = 'لاصق قائمة';
+
+$lang['g002_setting_randompic_periodicchange'] = "الفترة الدورية لتغيير الصورة";
+$lang['g002_setting_randompic_periodicchange_delay'] = "تأخير";
+$lang['g002_setting_randompic_periodicchange_deactivated'] = "معطل";
+$lang['g002_setting_randompic_height'] = "ارتفاع منطقة القائمة";
+$lang['g002_setting_randompic_height_auto'] = "تلقائي";
+
+// v2.1.3
+$lang['g002_cancel'] = "الغاء";
+$lang['g002_piwigo_default'] = "Piwigo الترتيب الافتراضي ل ";
+$lang['g002_apply_changes'] = "تطبيق التغييرات";
+$lang['g002_config_saved'] = "الاعدادات حفظت";
+$lang['g002_adviser_not_allowed'] = "المراقب لايسمح له بعمل تغييرات";
+$lang['g002_click_to_manage_rights'] = "تنطيم الحقوق";
+
+?>
