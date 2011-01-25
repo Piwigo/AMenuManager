2da40c3fe873483ea95c93987ae1f63edc9ffacd
ddtddt <ddtddt@piwigo.org>
2011-01-25 19:54:13 +0000

    [extensions] - AMenuManager - add no_NO (Norwegian) thanks to atlew
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@8892 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/no_NO/plugin.lang.php
@@ -0,0 +1,102 @@
+<?php
+
+$lang['Grum Plugin Classes is not installed'] = 'Pluggin <b>Grum Plugin Classes</b> er ikke installert';
+
+$lang['g002_title_page'] = 'Avansert Menu Behandler';
+$lang['g002_version'] = 'r';
+
+$lang['g002_error_invalid_ajax_call'] = "Ulovlig funksjons forespørsel !";
+
+$lang['g002_setmenu'] = 'Meny behandling';
+$lang['g002_addlinks'] = 'Linker';
+$lang['g002_randompict'] = 'Tilfeldig bilde';
+$lang['g002_personnalblock'] = 'Personalisert meny';
+
+$lang['g002_setmenu_nfo'] = 'Behandler visning av meny blokker';
+$lang['g002_addlinks_nfo'] = 'Meny blokk godtar å vise en liste med hyperlinker';
+$lang['g002_randompict_nfo'] = 'Meny blokker godtar å vise tilfeldig bilde fra galleriet';
+$lang['g002_personnalblock_nfo'] = 'Display personalised blocks into menu';
+
+
+$lang['g002_mode_new_window'] = 'Nytt vindu';
+$lang['g002_mode_current_window'] = 'Gjeldende vindu';
+$lang['g002_addlink'] = 'Legg til en link';
+$lang['g002_label'] = 'Etikett';
+$lang['g002_url'] = 'URL';
+$lang['g002_mode'] = 'Modus';
+$lang['g002_icon'] = 'Representativt bilde';
+$lang['g002_visible'] = 'Synlig';
+$lang['g002_linkslist'] = "Linker liste";
+$lang['g002_confirm_delete_link'] = "Slette link ?";
+$lang['g002_nolinks'] = 'Ingen linker';
+$lang['g002_link'] = 'link';
+$lang['g002_links'] = 'linker';
+
+$lang['g002_createoflink'] = 'Legg til link';
+$lang['g002_editoflink'] = 'Rediger link';
+$lang['g002_createthelink'] = 'Legg til link';
+$lang['g002_editthelink'] = 'Rediger link';
+
+$lang['g002_configlinks'] = 'Link instillinger';
+$lang['g002_setting_link_links']='Linker';
+$lang['g002_setting_link_show_icon'] = 'Vis linkers representativers bilder';
+
+$lang['g002_setting_block_menu'] = 'Meny integrasjon';
+$lang['g002_setting_block_active'] = 'Vis blokk i menyen';
+$lang['g002_setting_block_title'] = 'Blokk tittel';
+$lang['g002_apply'] = 'Legg til';
+
+$lang['g002_sectionslist'] = 'Meny blokker';
+
+$lang['g002_yesno_y'] = 'Ja';
+$lang['g002_yesno_n'] = 'Nei';
+
+$lang['g002_owner'] = 'Eier';
+$lang['g002_sectionid'] = 'Identifiser';
+$lang['g002_name'] = 'Etikett';
+
+$lang['g002_translate'] = 'Oversett med Google Translate';
+
+$lang['g002_configrandompic'] = 'Tilfelsig bilde instillinger';
+$lang['g002_setting_randompic_aboutpicture'] = 'Viste data';
+$lang['g002_setting_randompic_showname'] = 'Vis bilde navn';
+$lang['g002_setting_randompic_showcomment'] = 'Vis bilde kommentarer';
+$lang['g002_show_n'] = 'Nei';
+$lang['g002_show_o'] = 'Vis over bilde';
+$lang['g002_show_u'] = 'Vis under bilde';
+
+$lang['g002_personalisedlist'] = "Liste av personaliserte blokker";
+$lang['g002_notitle'] = '[Ingen tittel]';
+$lang['g002_title'] = 'Tittel';
+$lang['g002_sections'] = 'blokker';
+$lang['g002_section'] = 'blokk';
+$lang['g002_nosections'] = 'ingen blokker';
+$lang['g002_addsection'] = 'Legg til en ny blokk';
+$lang['g002_createofpersonalised'] = 'Lag en ny personlig blokk';
+$lang['g002_editofpersonalised'] = 'Endre en personlig blokk';
+$lang['g002_setting_block_langchoice'] = 'Språk valg';
+$lang['g002_setting_personalised_content'] = 'Blokk innhold';
+$lang['g002_setting_personalised_properties'] = 'Egenskaper';
+$lang['g002_setting_personalised_nfo'] = 'Beskrivelse';
+
+$lang['g002_all_languages'] = "Alle språk";
+
+$lang['g002_modmenu'] = '\'meny\' blokk';
+$lang['g002_modspecial'] = '\'spesial\' blokk';
+$lang['g002_labelmenu'] = 'Meny etikett';
+
+$lang['g002_setting_randompic_periodicchange'] = "Periodevis endring av bildet";
+$lang['g002_setting_randompic_periodicchange_delay'] = "Forsinkelse";
+$lang['g002_setting_randompic_periodicchange_deactivated'] = "Deaktivert";
+$lang['g002_setting_randompic_height'] = "Høyde på meny blokken";
+$lang['g002_setting_randompic_height_auto'] = "Automatisk";
+
+// v2.1.3
+$lang['g002_cancel'] = "Avbryt";
+$lang['g002_piwigo_default'] = "Piwigo satndard sortering";
+$lang['g002_apply_changes'] = "Aktiver forandringer";
+$lang['g002_config_saved'] = "Konfigurasjon er lagret";
+$lang['g002_adviser_not_allowed'] = "Rådgiver kan ikke gjøre endringer";
+$lang['g002_click_to_manage_rights'] = "Rettighets behandling";
+
+?>
