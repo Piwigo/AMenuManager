ab3035df8e84c0170388c55e1528139e01fc1238
ddtddt <ddtddt@piwigo.org>
2011-05-06 10:50:46 +0000

    [extensions] - AMenuManager - add SE thanks to berraw step 1
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@10788 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['Grum Plugin Classes is not installed'] = 'Plugin <b>Grum Plugin Classes</b> är inte installerade';
+$lang['g002_title_page'] = 'Avancerad menyhanterare';
+
+$lang['g002_version'] = 'r';
+
+$lang['g002_error_invalid_ajax_call'] = "felaktigt funktionsanrop !";
+
+$lang['g002_setmenu'] = 'Menyhanteraren';
+$lang['g002_addlinks'] = 'Länkar';
+$lang['g002_randompict'] = 'Slumpa fram en bild';
+$lang['g002_personnalblock'] = 'Personlig meny';
+
+$lang['g002_setmenu_nfo'] = 'Hantering av visade menyer/block';
+$lang['g002_addlinks_nfo'] = 'Menyer/ block tillåt visning av hyperlänkar';
+$lang['g002_randompict_nfo'] = 'Menyer/ block tillåt visning av slumpad bild från galleriet';
+$lang['g002_personnalblock_nfo'] = 'Visa personaliga blocks i  meny';
+
+
+?>
