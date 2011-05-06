0465c6634488eed5090ecfb54ccf5740e6803541
ddtddt <ddtddt@piwigo.org>
2011-05-06 12:00:28 +0000

    [extensions] - AMenuManager - add SE thanks to berraw step 2
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@10793 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/sv_SE/plugin.lang.php
+++ language/sv_SE/plugin.lang.php
@@ -16,6 +16,25 @@ $lang['g002_setmenu_nfo'] = 'Hantering av visade menyer/block';
 $lang['g002_addlinks_nfo'] = 'Menyer/ block tillåt visning av hyperlänkar';
 $lang['g002_randompict_nfo'] = 'Menyer/ block tillåt visning av slumpad bild från galleriet';
 $lang['g002_personnalblock_nfo'] = 'Visa personaliga blocks i  meny';
+$lang['g002_mode_new_window'] = 'Nytt fönster';
+$lang['g002_mode_current_window'] = 'Aktuellt fönster';
+$lang['g002_addlink'] = 'Lägg till en länk';
+$lang['g002_label'] = 'Etikett';
+$lang['g002_url'] = 'URL';
+$lang['g002_mode'] = 'Läge';
+$lang['g002_icon'] = 'Representativ bild';
+$lang['g002_visible'] = 'Synlig';
+$lang['g002_linkslist'] = 'Länklista';
+$lang['g002_confirm_delete_link'] = 'Vill du ta bort länken?';
+$lang['g002_nolinks'] = 'Inga länkar';
+$lang['g002_link'] = 'länk';
+$lang['g002_links'] = 'länkar';
+$lang['g002_createoflink'] = 'Lägg till länk';
+$lang['g002_editoflink'] = 'Redigera länk';
+$lang['g002_createthelink'] = 'Lägg till länk';
+$lang['g002_editthelink'] = 'Redigera länk';
+$lang['g002_configlinks'] = 'Länkinställningar';
+$lang['g002_setting_link_links'] = 'Länkar'; 
 
 
 ?>
