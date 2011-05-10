2138374317cd7a900cf6ba8ba61df5e701b3d66f
ddtddt <ddtddt@piwigo.org>
2011-05-10 20:04:39 +0000

    [extensions] - AMenuManager - add SE thanks to berraw step 3
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@10843 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/sv_SE/plugin.lang.php
+++ language/sv_SE/plugin.lang.php
@@ -1,8 +1,8 @@
 <?php
 
 $lang['Grum Plugin Classes is not installed'] = 'Plugin <b>Grum Plugin Classes</b> är inte installerade';
-$lang['g002_title_page'] = 'Avancerad menyhanterare';
 
+$lang['g002_title_page'] = 'Avancerad menyhanterare';
 $lang['g002_version'] = 'r';
 
 $lang['g002_error_invalid_ajax_call'] = "felaktigt funktionsanrop !";
@@ -16,6 +16,8 @@ $lang['g002_setmenu_nfo'] = 'Hantering av visade menyer/block';
 $lang['g002_addlinks_nfo'] = 'Menyer/ block tillåt visning av hyperlänkar';
 $lang['g002_randompict_nfo'] = 'Menyer/ block tillåt visning av slumpad bild från galleriet';
 $lang['g002_personnalblock_nfo'] = 'Visa personaliga blocks i  meny';
+
+
 $lang['g002_mode_new_window'] = 'Nytt fönster';
 $lang['g002_mode_current_window'] = 'Aktuellt fönster';
 $lang['g002_addlink'] = 'Lägg till en länk';
@@ -29,12 +31,53 @@ $lang['g002_confirm_delete_link'] = 'Vill du ta bort länken?';
 $lang['g002_nolinks'] = 'Inga länkar';
 $lang['g002_link'] = 'länk';
 $lang['g002_links'] = 'länkar';
+
 $lang['g002_createoflink'] = 'Lägg till länk';
 $lang['g002_editoflink'] = 'Redigera länk';
 $lang['g002_createthelink'] = 'Lägg till länk';
 $lang['g002_editthelink'] = 'Redigera länk';
+
 $lang['g002_configlinks'] = 'Länkinställningar';
-$lang['g002_setting_link_links'] = 'Länkar'; 
+$lang['g002_setting_link_links'] = 'Länkar';
+$lang['g002_setting_link_show_icon'] = 'Visa länkar som representer bilden';
+
+$lang['g002_setting_block_menu'] = 'Menyintegrering';
+$lang['g002_setting_block_active'] = 'Visa block i meny';
+$lang['g002_setting_block_title'] = 'Blockets titel';
+$lang['g002_apply'] = 'Applicera';
+
+$lang['g002_sectionslist'] = 'Menyer block';
+
+$lang['g002_yesno_y'] = 'Ja';
+$lang['g002_yesno_n'] = 'Nej';
+
+$lang['g002_owner'] = 'Ägare';
+$lang['g002_sectionid'] = 'Identifierare';
+$lang['g002_name'] = 'Etikett';
+
+$lang['g002_translate'] = 'Översätt med Google Translate';
+
+$lang['g002_configrandompic'] = 'Inställningar slumpvisa bilder';
+$lang['g002_setting_randompic_aboutpicture'] = 'Visad information';
+$lang['g002_setting_randompic_showname'] = 'Visa bildens namn';
+$lang['g002_setting_randompic_showcomment'] = 'Visa bildens kommentarer';
+$lang['g002_show_n'] = 'Nej';
+$lang['g002_show_o'] = 'Visa över bilden';
+$lang['g002_show_u'] = 'Visa under bilden';
+
+$lang['g002_personalisedlist'] = 'Lista som visar personliga block';
+$lang['g002_notitle'] = '[Ingen titel]';
+$lang['g002_title'] = 'Titel';
+$lang['g002_sections'] = 'block';
+$lang['g002_section'] = 'block';
+$lang['g002_nosections'] = 'Inga block';
+$lang['g002_addsection'] = 'Addera nytt block';
+$lang['g002_createofpersonalised'] = 'Skapa nytt personligt block';
+$lang['g002_editofpersonalised'] = 'Ändra ett personligt block';
+$lang['g002_setting_block_langchoice'] = 'Språkval';
+$lang['g002_setting_personalised_content'] = 'Blockinnehåll';
+$lang['g002_setting_personalised_properties'] = 'Egenskaper';
+$lang['g002_setting_personalised_nfo'] = 'Beskrivning'; 
 
 
 ?>
