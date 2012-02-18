35dfefb42b1c22fdbe42e793aafeee2086ed7479
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-02-18 20:43:48 +0000

    [AMenuManager] Update uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@13239 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,120 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['g002_accessibility'] = 'Доступність';
+$lang['g002_addlink'] = 'Додати посилання';
+$lang['g002_addlinks'] = 'Посилання';
+$lang['g002_addlinks_nfo'] = 'Меню в блоку дозволяє відображати список гіперпосилання';
+$lang['g002_addsection'] = 'Додати новий блок';
+$lang['g002_adviser_not_allowed'] = 'Радник не має права вносити зміни';
+$lang['g002_album'] = 'Альбом &rArr;меню';
+$lang['g002_album2menu'] = 'Перетворення альбому в меню';
+$lang['g002_album_nfo'] = 'Дозволяє перетворювати один або кілька альбомів в меню';
+$lang['g002_all_languages'] = 'Всі мови';
+$lang['g002_apply'] = 'Застосовувати';
+$lang['g002_apply_changes'] = 'Застосувати зміни';
+$lang['g002_cancel'] = 'Скасувати';
+$lang['g002_click_to_manage_rights'] = 'Управління правами';
+$lang['g002_configlinks'] = 'Налаштування посилань';
+$lang['g002_configrandompic'] = 'Випадкові налаштування зображення';
+$lang['g002_config_not_saved'] = 'Налаштування не були збережені';
+$lang['g002_config_saved'] = 'Конфігурація збережена';
+$lang['g002_confirm_delete_link'] = 'Видалити посилання?';
+$lang['g002_createoflink'] = 'Додати посилання';
+$lang['g002_createofpersonalised'] = 'Створіть новий персоналізований блок';
+$lang['g002_createthelink'] = 'Додати посилання';
+$lang['g002_editoflink'] = 'Редагувати посилання';
+$lang['g002_editofpersonalised'] = 'Змінити персональний блок';
+$lang['g002_editthelink'] = 'Редагувати посилання';
+$lang['g002_error_invalid_ajax_call'] = 'Невірний виклик функції!';
+$lang['g002_icon'] = 'Типова фотографія';
+$lang['g002_label'] = 'Позначка';
+$lang['g002_labelmenu'] = 'Меню на позначці';
+$lang['g002_link'] = 'посилання';
+$lang['g002_links'] = 'посилання';
+$lang['g002_linkslist'] = 'Список посилань';
+$lang['g002_loading'] = 'Завантаження...';
+$lang['g002_mode'] = 'Режим';
+$lang['g002_mode_current_window'] = 'Поточне вікно';
+$lang['g002_mode_new_window'] = 'У новому вікні';
+$lang['g002_modmenu'] = 'блок \'меню\'';
+$lang['g002_modspecial'] = 'блок \'спеціальний\'';
+$lang['g002_name'] = 'Позначка';
+$lang['g002_nolinks'] = 'Немає жодного посилання';
+$lang['g002_nosections'] = 'Немає блоків';
+$lang['g002_notitle'] = '[Без назви]';
+$lang['g002_ok'] = 'ОК';
+$lang['g002_owner'] = 'Власник';
+$lang['g002_personalisedlist'] = 'Список персоналізованих блоків';
+$lang['g002_personnalblock'] = 'Індивідуальне меню';
+$lang['g002_personnalblock_nfo'] = 'Відображення персоналізованих блоків в меню';
+$lang['g002_piwigo_default'] = 'Piwigo сортування за замовчуванням';
+$lang['g002_plugin'] = 'Плаґін';
+$lang['g002_randompict'] = 'Випадкова фотографія';
+$lang['g002_randompict_nfo'] = 'Меню в блоку дозволяє відображати випадкові зображення з галереї';
+$lang['g002_reset_order'] = 'Скинути порядок';
+$lang['g002_section'] = 'блок';
+$lang['g002_sectionid'] = 'Ідентифікатор';
+$lang['g002_sections'] = 'блоки';
+$lang['g002_sectionslist'] = 'Меню блоків';
+$lang['g002_selectedpict'] = 'Вибрані фотографії';
+$lang['g002_selected_all_gallery'] = 'Всі альбоми';
+$lang['g002_selected_categories'] = 'Всі вибрані альбоми';
+$lang['g002_selected_favorites_wm'] = 'Обрані фотографії веб-майстрів';
+$lang['g002_setmenu'] = 'Меню управління';
+$lang['g002_setmenu_nfo'] = 'Управління блоками відображається в меню';
+$lang['g002_setting_albums_menus'] = 'Альбоми, щоб перетворити в меню';
+$lang['g002_setting_blocks_position'] = 'Видимість та упорядкування меню';
+$lang['g002_setting_block_active'] = 'Відображення блоку в меню';
+$lang['g002_setting_block_langchoice'] = 'Вибір мови';
+$lang['g002_setting_block_menu'] = 'Меню інтеграції';
+$lang['g002_setting_block_title'] = 'Назва блоку';
+$lang['g002_setting_core_blocks_content'] = 'Вміст для \'Меню і Спеціальні\' меню';
+$lang['g002_setting_link_links'] = 'Посилання';
+$lang['g002_setting_link_show_icon'] = 'Відображати посилання типової фотографії';
+$lang['g002_setting_personalised_content'] = 'Вміст блоку';
+$lang['g002_setting_personalised_nfo'] = 'Опис';
+$lang['g002_setting_personalised_properties'] = 'Властивості';
+$lang['g002_setting_randompic_aboutpicture'] = 'Показати дані';
+$lang['g002_setting_randompic_height'] = 'Висота блоку меню';
+$lang['g002_setting_randompic_height_auto'] = 'Автоматично';
+$lang['g002_setting_randompic_periodicchange'] = 'Періодична зміна зображення';
+$lang['g002_setting_randompic_periodicchange_deactivated'] = 'Вимкнений';
+$lang['g002_setting_randompic_periodicchange_delay'] = 'Затримувати';
+$lang['g002_setting_randompic_showcomment'] = 'Показати опис фотографії';
+$lang['g002_setting_randompic_showname'] = 'Показати назву фотографії';
+$lang['g002_show_n'] = 'Немає';
+$lang['g002_show_o'] = 'Відображення над фотографією';
+$lang['g002_show_u'] = 'Відображення під фотографією';
+$lang['g002_title'] = 'Назва';
+$lang['g002_title_page'] = 'Розширене управління меню';
+$lang['g002_translate'] = 'Перекласти з Google Translate';
+$lang['g002_url'] = 'URL';
+$lang['g002_valid_order'] = 'Застосувати порядок';
+$lang['g002_version'] = 'Версія';
+$lang['g002_visible'] = 'Видимий';
+$lang['g002_yesno_n'] = 'Ні';
+$lang['g002_yesno_y'] = 'Так';
+$lang['gmaps_gpc_not_up_to_date'] = 'Це необхідно для плаґіна, який <i>Grum Plugin Classes</i> версії %s встановлений.
+В даний час у вас встановлена версія  %s : перейдіть з оновленням версії плаґіна <i>Grum Plugin Classes</i>.';
+$lang['Grum Plugin Classes is not installed'] = 'Плаґін <b>Grum Plugin Classes</b> не встановлено';
+?>
