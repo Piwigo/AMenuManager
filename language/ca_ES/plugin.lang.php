2badfd4e0db605457e6094c441e92485544b0c14
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-27 13:46:19 +0000

    [amenumanager] Update ca_ES, thanks to : AlbertParera
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@14476 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Grum Plugin Classes is not installed'] = 'El complement <b>Grum Plugin Classes</b> no està instal·lat';
+$lang['g002_addlinks'] = 'Enllaços';
+$lang['g002_error_invalid_ajax_call'] = 'Petició no vàlid!';
+$lang['g002_personnalblock'] = 'Menú personalitzat';
+$lang['g002_randompict'] = 'Imatge a l\'atzar';
+$lang['g002_setmenu_nfo'] = 'Gestiona els blocs del menú visualitzat';
+$lang['g002_setmenu'] = 'Gestió del menú';
+$lang['g002_title_page'] = 'Gestió del Menú Avançat';
+$lang['g002_version'] = 'r';
+?>
