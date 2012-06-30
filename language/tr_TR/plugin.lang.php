b2dc01f49a01f95dce4eacd409b11c155245ccc5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-30 12:52:50 +0000

    [amenumanager] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@16222 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,119 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Grum Plugin Classes is not installed'] = '<b>Grum Plugin Classes</b> eklentisi yüklü değil';
+$lang['g002_accessibility'] = 'Erişebilirlik';
+$lang['g002_addlinks_nfo'] = 'Menü bloğu hyperlink listesi görüntülenmesine izin veriyor';
+$lang['g002_addlinks'] = 'Bağlantılar';
+$lang['g002_addlink'] = 'Bir bağlantı ekle';
+$lang['g002_addsection'] = 'Yeni bir blok ekle';
+$lang['g002_adviser_not_allowed'] = 'Danışman değişiklik yapmaya izin vermiyor';
+$lang['g002_album2menu'] = 'Albüm menüye dönüştürüldü';
+$lang['g002_album_nfo'] = 'Bir veya daha fazla albümün bir menüye dönüştürülmesine izin verir';
+$lang['g002_album'] = 'Albüm &rArr; menüsü';
+$lang['g002_all_languages'] = 'Tüm diller';
+$lang['g002_apply_changes'] = 'Değişiklikleri uygula';
+$lang['g002_apply'] = 'Uygula';
+$lang['g002_cancel'] = 'Vazgeç';
+$lang['g002_click_to_manage_rights'] = 'Hakların yönetimi';
+$lang['g002_config_not_saved'] = 'Yapılandırma kaydedilmedi';
+$lang['g002_config_saved'] = 'Yapılandırma kaydedildi';
+$lang['g002_configlinks'] = 'Bağlantı ayarları';
+$lang['g002_configrandompic'] = 'Rastgele resim ayarları';
+$lang['g002_confirm_delete_link'] = 'Bağlantı silinsin mi ?';
+$lang['g002_createoflink'] = 'Bağlantı ekle';
+$lang['g002_createofpersonalised'] = 'Yeni bir kişisel blok oluştur';
+$lang['g002_createthelink'] = 'Bağlantı ekle';
+$lang['g002_editoflink'] = 'Bağlantı düzenle';
+$lang['g002_editofpersonalised'] = 'Kişisel bir bloğu değiştir';
+$lang['g002_editthelink'] = 'Bağlantı düzenle';
+$lang['g002_error_invalid_ajax_call'] = 'Geçersiz işlev çağrısı!';
+$lang['g002_icon'] = 'Temsili resim';
+$lang['g002_labelmenu'] = 'Menü\'nün etiketi';
+$lang['g002_label'] = 'Etiket';
+$lang['g002_linkslist'] = 'Bağlantı listesi';
+$lang['g002_links'] = 'bağlantı';
+$lang['g002_link'] = 'bağlantı';
+$lang['g002_loading'] = 'Yükleniyor...';
+$lang['g002_mode_current_window'] = 'Geçerli pencere';
+$lang['g002_mode_new_window'] = 'Yeni pencere';
+$lang['g002_mode'] = 'Mod';
+$lang['g002_modmenu'] = '\'menu\' bloğu';
+$lang['g002_modspecial'] = '\'special\' blok';
+$lang['g002_name'] = 'Etiket';
+$lang['g002_nolinks'] = 'Bağlantı yok';
+$lang['g002_nosections'] = 'Blok yok';
+$lang['g002_notitle'] = '[Etiketsiz]';
+$lang['g002_ok'] = 'TAMAM';
+$lang['g002_owner'] = 'Sahip';
+$lang['g002_personalisedlist'] = 'Kişiselleştirilmiş blok listesi';
+$lang['g002_personnalblock_nfo'] = 'Menüde kişiselleştirilmiş blok görüntüle';
+$lang['g002_personnalblock'] = 'Kişiselleştirilmiş menü';
+$lang['g002_piwigo_default'] = 'Piwigo varsayılan sıralaması';
+$lang['g002_plugin'] = 'Eklenti';
+$lang['g002_randompict_nfo'] = 'Menü\'nün bloğu, galeriden rastgele resim görüntülenmesine izin verir';
+$lang['g002_randompict'] = 'Rastgele resim';
+$lang['g002_reset_order'] = 'Sıralamayı sıfırla';
+$lang['g002_sectionid'] = 'Tanımlayıcı';
+$lang['g002_sectionslist'] = 'Menü\'nün bloğu';
+$lang['g002_sections'] = 'blok';
+$lang['g002_section'] = 'blok';
+$lang['g002_selected_all_gallery'] = 'Tüm albümler';
+$lang['g002_selected_categories'] = 'Tüm seçili albümler';
+$lang['g002_selected_favorites_wm'] = 'Webmaster\'ın favori resimleri';
+$lang['g002_selectedpict'] = 'Seçili resimler';
+$lang['g002_setmenu_nfo'] = 'Görüntülenen menü bloğunun yönetimi';
+$lang['g002_setmenu'] = 'Menü yönetimi';
+$lang['g002_setting_albums_menus'] = 'Menüye dönüşütürülecek albümler';
+$lang['g002_setting_block_active'] = 'Menüde blok görüntüle';
+$lang['g002_setting_block_langchoice'] = 'Dil seçimi';
+$lang['g002_setting_block_menu'] = 'Menü birleştirme';
+$lang['g002_setting_block_title'] = 'Blok etiketi';
+$lang['g002_setting_blocks_position'] = 'Menülerin görünürlüğü ve sıralaması';
+$lang['g002_setting_core_blocks_content'] = '\'Menu\' ve \'Specials\' menüleri içeriği';
+$lang['g002_setting_link_links'] = 'Bağlantılar';
+$lang['g002_setting_link_show_icon'] = 'Bağlantı temsili resmini görüntüle';
+$lang['g002_setting_personalised_content'] = 'Blok içeriği';
+$lang['g002_setting_personalised_nfo'] = 'Açıklama';
+$lang['g002_setting_personalised_properties'] = 'Özellikler';
+$lang['g002_setting_randompic_aboutpicture'] = 'Görüntülenen bilgiler';
+$lang['g002_setting_randompic_height_auto'] = 'Otomatik';
+$lang['g002_setting_randompic_height'] = 'Menü bloğu yüksekliği';
+$lang['g002_setting_randompic_periodicchange_deactivated'] = 'Etkin değil';
+$lang['g002_setting_randompic_periodicchange_delay'] = 'Gecikme';
+$lang['g002_setting_randompic_periodicchange'] = 'Resmin periyodik değişimi';
+$lang['g002_setting_randompic_showcomment'] = 'Resim yorumunu göster';
+$lang['g002_setting_randompic_showname'] = 'Resim adını göster';
+$lang['g002_show_n'] = 'Hayır';
+$lang['g002_show_o'] = 'Resim üzerinde göster';
+$lang['g002_show_u'] = 'Resim altında görüntüle';
+$lang['g002_title_page'] = 'Gelişmiş Menü Yönetimi';
+$lang['g002_title'] = 'Etiket';
+$lang['g002_url'] = 'URL';
+$lang['g002_valid_order'] = 'Sıralamayı uygula';
+$lang['g002_version'] = 'r';
+$lang['g002_visible'] = 'Görünür';
+$lang['g002_yesno_n'] = 'Hayır';
+$lang['g002_yesno_y'] = 'Evet';
+$lang['gmaps_gpc_not_up_to_date'] = 'Eklenti için <i>Grum Plugin Classes</i> sürüm %s \'in kurulu olması gereklidir.
+Şu anda sürüm %s kuruludur. Lütfen <i>Grum Plugin Classes</i> eklentisinin güncelleme sürümü ile devam ediniz.';
+?>
