852bdfe2dfc402e5e236911a7a3b846f3e44322f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-14 22:03:50 +0000

    [amenumanager] Update hu_HU, thanks to : samli
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@16735 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/hu_HU/plugin.lang.php
+++ language/hu_HU/plugin.lang.php
@@ -1,8 +1,8 @@
 
 $lang['g002_title_page'] = 'Speciális menükezelő';
-$lang['g002_version'] = 'r';
+$lang['g002_version'] = 'v';
 
-$lang['g002_error_invalid_ajax_call'] = "Érvénytelen függvényhívás !";
+$lang['g002_error_invalid_ajax_call'] = 'Érvénytelen függvényhívás !';
 
 $lang['g002_setmenu'] = 'Menü kezelése';
 $lang['g002_addlinks'] = 'Linkek';
@@ -23,8 +23,8 @@ $lang['g002_url'] = 'URL';
 $lang['g002_mode'] = 'Megnyitás';
 $lang['g002_icon'] = 'Bemutató kép';
 $lang['g002_visible'] = 'Felsorolás';
-$lang['g002_linkslist'] = "Linkek listája";
-$lang['g002_confirm_delete_link'] = "Link törlése ?";
+$lang['g002_linkslist'] = 'Linkek listája';
+$lang['g002_confirm_delete_link'] = 'Link törlése ?';
 $lang['g002_nolinks'] = 'Nincsenek linkek';
 $lang['g002_link'] = 'link';
 $lang['g002_links'] = 'linkek';
@@ -34,7 +34,7 @@ $lang['g002_createthelink'] = 'Link hozzáadása';
 $lang['g002_editthelink'] = 'Link javítása';
 
 $lang['g002_configlinks'] = 'Linkek menüelem beállításai';
-$lang['g002_setting_link_links']='Linkek';
+$lang['g002_setting_link_links'] = 'Linkek';
 $lang['g002_setting_link_show_icon'] = 'Linkhez rendelt alapértelmezett kép';
 
 $lang['g002_setting_block_menu'] = 'Menü integráció';
@@ -59,7 +59,7 @@ $lang['g002_show_n'] = 'Nem';
 $lang['g002_show_o'] = 'Megjelenítés a kép felett';
 $lang['g002_show_u'] = 'Megjelenítés a kép alatt';
 
-$lang['g002_personalisedlist'] = "Egyedi blokkok listája";
+$lang['g002_personalisedlist'] = 'Egyedi blokkok listája';
 $lang['g002_notitle'] = '[Nincs cím]';
 $lang['g002_title'] = 'Cím';
 $lang['g002_sections'] = 'blokkok';
@@ -73,18 +73,44 @@ $lang['g002_setting_personalised_content'] = 'Blokk tartalma';
 $lang['g002_setting_personalised_properties'] = 'Tulajdonságok';
 $lang['g002_setting_personalised_nfo'] = 'Leírás';
 
-$lang['g002_all_languages'] = "Összes nyelv";
+$lang['g002_all_languages'] = 'Összes nyelv';
 
 $lang['g002_modmenu'] = '\'Menü\' blokk';
 $lang['g002_modspecial'] = '\'Speciális\' blokk';
-$lang['g002_labelmenu'] = 'Menü címkék';
+$lang['g002_labelmenu'] = 'Menü cimkék';
 
-$lang['g002_setting_randompic_periodicchange'] = 'Periodikus képváltás beállítása';
-$lang['g002_album2menu'] = "Album menüvé konvertálása";
-$lang['g002_album'] = 'Album ⇒ menü';
-$lang['g002_setting_albums_menus'] = "Menüvé konvertálandó albumok";
+$lang['g002_setting_randompic_periodicchange'] = 'Periódikus képváltás beállítása';
+$lang['g002_album2menu'] = 'Album menüvé konvertálása';
+$lang['g002_album'] = 'Album &rArr; menü';
+$lang['g002_setting_albums_menus'] = 'Menüvé konvertálandó albumok';
 $lang['gmaps_gpc_not_up_to_date'] = 'A bővítmény használatához szükség van a <i>Grum Plugin Classes</i> %s változatának telepítésére.
 Jelenleg a %s változat van telepítve. Lépjen tovább és frissítse a <i>Grum Plugin Classes</i> bővítményt.';
 Jelenleg a %s változat van telepítve. Lépj tovább és frissítsd a <i>Grum Plugin Classes</i> bővítményt.";
 
+$lang['Grum Plugin Classes is not installed'] = 'Szükség van a <b>Grum Plugin Classes</b> bővítményre, telepítenie kell';
+$lang['g002_accessibility'] = 'Láthatja:';
+$lang['g002_adviser_not_allowed'] = 'Nincs jogosultság a módosításhoz';
+$lang['g002_album_nfo'] = 'Egy vagy több albumból menü készítése';
+$lang['g002_apply_changes'] = 'Alkalmaz';
+$lang['g002_cancel'] = 'Mégse';
+$lang['g002_click_to_manage_rights'] = 'Jogkezelés';
+$lang['g002_config_not_saved'] = 'A konfiguráció nem került mentésre';
+$lang['g002_config_saved'] = 'Konfiguráció mentve';
+$lang['g002_editoflink'] = 'Beállítások módosítása';
+$lang['g002_loading'] = 'Töltés...';
+$lang['g002_ok'] = 'OK';
+$lang['g002_piwigo_default'] = 'Piwigo alapértelmezett rendezés';
+$lang['g002_plugin'] = 'Bővítmény';
+$lang['g002_reset_order'] = 'Sorrend alaphelyzetbe';
+$lang['g002_selected_all_gallery'] = 'Összes album';
+$lang['g002_selected_categories'] = 'Összes kijelölt album';
+$lang['g002_selected_favorites_wm'] = 'Webmester kedvenc képei';
+$lang['g002_selectedpict'] = 'Kijelölt képek';
+$lang['g002_setting_blocks_position'] = 'Láthatóság és menü sorrend ';
+$lang['g002_setting_core_blocks_content'] = '\'Menü\' és \'Speciális összetevők\'';
+$lang['g002_setting_randompic_height_auto'] = 'Automatikus';
+$lang['g002_setting_randompic_height'] = 'Menü blokkok szélessége';
+$lang['g002_setting_randompic_periodicchange_deactivated'] = 'Kikapcsolva';
+$lang['g002_setting_randompic_periodicchange_delay'] = 'Képváltás ideje';
+$lang['g002_valid_order'] = 'Sorrend alkalmazása';
 ?>
