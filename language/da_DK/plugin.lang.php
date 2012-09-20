9e2c2379ad04d94c7ca6688be55c026bb811fd16
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-20 16:01:44 +0000

    [amenumanager] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@18069 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,119 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['g002_setting_randompic_periodicchange_delay'] = 'Forsinkelse';
+$lang['g002_setting_randompic_showcomment'] = 'Vis billedes kommentar';
+$lang['g002_setting_randompic_showname'] = 'Vis billedes navn';
+$lang['g002_show_n'] = 'Nej';
+$lang['g002_show_o'] = 'Vis over billedet';
+$lang['g002_show_u'] = 'Vis under billedet';
+$lang['g002_title'] = 'Titel';
+$lang['g002_title_page'] = 'Advanced Menu Management';
+$lang['g002_url'] = 'URL';
+$lang['g002_valid_order'] = 'Udfør rækkefølge';
+$lang['g002_version'] = 'r';
+$lang['g002_visible'] = 'Synlig';
+$lang['g002_yesno_n'] = 'Nej';
+$lang['g002_yesno_y'] = 'Ja';
+$lang['gmaps_gpc_not_up_to_date'] = 'Plugin\'et har brug for at <i>Grum Plugin Classes</i> version %s er installeret.
+I øjeblikket er version %s installeret: fortsæt med opdateringsversionen af plugin\'en  <i>Grum Plugin Classes</i>.';
+$lang['g002_selectedpict'] = 'Valgte billede';
+$lang['g002_setmenu'] = 'Menuhåndtering';
+$lang['g002_setmenu_nfo'] = 'Håndtering af vist menus blokke';
+$lang['g002_setting_albums_menus'] = 'Albummer der skal konverteres til menu';
+$lang['g002_setting_block_active'] = 'Vis blok i menu';
+$lang['g002_setting_block_langchoice'] = 'Sprogvalg';
+$lang['g002_setting_block_menu'] = 'Menuintegration';
+$lang['g002_setting_block_title'] = 'Bloktitel';
+$lang['g002_setting_blocks_position'] = 'Synlighed og rækkefølge vedrørende menuer';
+$lang['g002_setting_core_blocks_content'] = 'Indhold til menuerne \'Menu\' og \'Specials\'';
+$lang['g002_setting_link_links'] = 'Links';
+$lang['g002_setting_link_show_icon'] = 'Vis links repræsentative billede';
+$lang['g002_setting_personalised_content'] = 'Bloks indhold';
+$lang['g002_setting_personalised_nfo'] = 'Beskrivelse';
+$lang['g002_setting_personalised_properties'] = 'Egenskaber';
+$lang['g002_setting_randompic_aboutpicture'] = 'Viste data';
+$lang['g002_setting_randompic_height'] = 'Menubloks højde';
+$lang['g002_setting_randompic_height_auto'] = 'Automatisk';
+$lang['g002_setting_randompic_periodicchange'] = 'Periodisk udskiftning af billede';
+$lang['g002_setting_randompic_periodicchange_deactivated'] = 'Deaktiveret';
+$lang['g002_nolinks'] = 'Ingen links';
+$lang['g002_nosections'] = 'Ingen blokke';
+$lang['g002_notitle'] = '[Ingen titel]';
+$lang['g002_ok'] = 'OK';
+$lang['g002_owner'] = 'Ejer';
+$lang['g002_personalisedlist'] = 'Liste over personaliserede blokke';
+$lang['g002_personnalblock'] = 'Personaliseret menu';
+$lang['g002_personnalblock_nfo'] = 'Vis personaliserede blokke i menu';
+$lang['g002_piwigo_default'] = 'Piwigos standardrækkefølge';
+$lang['g002_plugin'] = 'Plugin';
+$lang['g002_randompict'] = 'Tilfældigt billede';
+$lang['g002_randompict_nfo'] = 'Menuens blok må vise et tilfældigt billede fra galleriet';
+$lang['g002_reset_order'] = 'Nulstil rækkefølge';
+$lang['g002_section'] = 'blok';
+$lang['g002_sectionid'] = 'Identifikator';
+$lang['g002_sections'] = 'blokke';
+$lang['g002_sectionslist'] = 'Menus blokke';
+$lang['g002_selected_all_gallery'] = 'Alle albummer';
+$lang['g002_selected_categories'] = 'Alle valgte albummer';
+$lang['g002_selected_favorites_wm'] = 'Webmasters yndlingsbilleder';
+$lang['g002_createoflink'] = 'Tilføj link';
+$lang['g002_createofpersonalised'] = 'Opret en ny personaliseret blok';
+$lang['g002_createthelink'] = 'Tiløj link';
+$lang['g002_editoflink'] = 'Rediger link';
+$lang['g002_editofpersonalised'] = 'Foretag ændring af personaliseret blok';
+$lang['g002_editthelink'] = 'Rediger link';
+$lang['g002_error_invalid_ajax_call'] = 'Ugyldigt funktionskald!';
+$lang['g002_icon'] = 'Repræsentativt billede';
+$lang['g002_label'] = 'Ettiket';
+$lang['g002_labelmenu'] = 'Menus ettiket';
+$lang['g002_link'] = 'link';
+$lang['g002_links'] = 'links';
+$lang['g002_linkslist'] = 'Linkliste';
+$lang['g002_loading'] = 'Indlæser ...';
+$lang['g002_mode'] = 'Tilstand';
+$lang['g002_mode_current_window'] = 'Aktuelt vindue';
+$lang['g002_mode_new_window'] = 'Nyt vindue';
+$lang['g002_modmenu'] = '\'menu\'-blok';
+$lang['g002_modspecial'] = '\'special\'-blok';
+$lang['g002_name'] = 'Ettiket';
+$lang['Grum Plugin Classes is not installed'] = 'Plugin\'en <b>Grum Plugin Classes</b> er ikke installeret';
+$lang['g002_accessibility'] = 'Tilgængelighed';
+$lang['g002_addlink'] = 'Tilføj et link';
+$lang['g002_addlinks'] = 'Links';
+$lang['g002_addlinks_nfo'] = 'Menuens blok må vise en liste over hyperlinks';
+$lang['g002_addsection'] = 'Tilføj en ny blok';
+$lang['g002_adviser_not_allowed'] = 'Rådgiver har ikke tilladelse til at foretage ændringer';
+$lang['g002_album'] = 'Album &rArr; menu';
+$lang['g002_album2menu'] = 'Konverterede album til menu';
+$lang['g002_album_nfo'] = 'Giv tilladelse til at konvertere et eller flere albummer til en menu';
+$lang['g002_all_languages'] = 'Alle sprog';
+$lang['g002_apply'] = 'Udfør';
+$lang['g002_apply_changes'] = 'Udfør ændringer';
+$lang['g002_cancel'] = 'Afbryd';
+$lang['g002_click_to_manage_rights'] = 'Rettighedshåndtering';
+$lang['g002_config_not_saved'] = 'Opsætning blev ikke gemt';
+$lang['g002_config_saved'] = 'Opsætning gemt';
+$lang['g002_configlinks'] = 'Linksopsætning';
+$lang['g002_configrandompic'] = 'Opsætning af tilfældigt billede';
+$lang['g002_confirm_delete_link'] = 'Slet link?';
+?>
