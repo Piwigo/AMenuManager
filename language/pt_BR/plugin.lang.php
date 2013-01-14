1a1d32c71800b3881e4ccefef596911bcec44832
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-14 05:36:45 +0000

    [amenumanager] Add pt_BR, thanks to : oigreslima
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@20115 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,119 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['g002_setting_randompic_periodicchange_delay'] = 'Atraso';
+$lang['g002_setting_randompic_showcomment'] = 'Mostra comentário da foto';
+$lang['g002_setting_randompic_showname'] = 'Mostra nome da foto';
+$lang['g002_show_n'] = 'Não';
+$lang['g002_show_o'] = 'Mostra sobre a foto';
+$lang['g002_show_u'] = 'Mostra sob a foto';
+$lang['g002_title'] = 'Título';
+$lang['g002_title_page'] = 'Gerenciamento avançado de Menu';
+$lang['g002_url'] = 'URL';
+$lang['g002_valid_order'] = 'Aplica ordem';
+$lang['g002_version'] = 'r';
+$lang['g002_visible'] = 'Visível';
+$lang['g002_yesno_n'] = 'Não';
+$lang['g002_yesno_y'] = 'Sim';
+$lang['gmaps_gpc_not_up_to_date'] = 'É necessário para o plugin que a versão %s do <i>Grum Plugin Classes</i> esteja instalada.
+Atualmente a versão %s está instalada; por favor proceda  a atualização do plugin <i>Grum Plugin Classes</i>.';
+$lang['g002_selectedpict'] = 'Selecione fotos';
+$lang['g002_setmenu'] = 'Gerenciamento de Menus';
+$lang['g002_setmenu_nfo'] = 'Gerenciamento de bloco de menus a ser mostrado';
+$lang['g002_setting_albums_menus'] = 'Albuns convertem para menus';
+$lang['g002_setting_block_active'] = 'Mostra blocos no menu';
+$lang['g002_setting_block_langchoice'] = 'Escolhe idioma';
+$lang['g002_setting_block_menu'] = 'Integração de menus';
+$lang['g002_setting_block_title'] = 'Título de blocos';
+$lang['g002_setting_blocks_position'] = 'Visibilidade e ordem dos menus';
+$lang['g002_setting_core_blocks_content'] = 'Conteúdo para "Menu" & Menu \'Especial\'';
+$lang['g002_setting_link_links'] = 'Links';
+$lang['g002_setting_link_show_icon'] = 'Mostra links de fotos representativas';
+$lang['g002_setting_personalised_content'] = 'Conteúdo do Bloco';
+$lang['g002_setting_personalised_nfo'] = 'Descrição';
+$lang['g002_setting_personalised_properties'] = 'Propriedades';
+$lang['g002_setting_randompic_aboutpicture'] = 'Mostra dados';
+$lang['g002_setting_randompic_height'] = 'Altura do bloco de menu';
+$lang['g002_setting_randompic_height_auto'] = 'Automático';
+$lang['g002_setting_randompic_periodicchange'] = 'Mudança periódica de imagem';
+$lang['g002_setting_randompic_periodicchange_deactivated'] = 'Desabilitado';
+$lang['g002_nolinks'] = 'Sem links';
+$lang['g002_nosections'] = 'Sem blocos';
+$lang['g002_notitle'] = '[Sem Título]';
+$lang['g002_ok'] = 'Ok';
+$lang['g002_owner'] = 'Proprietário';
+$lang['g002_personalisedlist'] = 'Lista de blocos personalizados';
+$lang['g002_personnalblock'] = 'Menu Personalizado';
+$lang['g002_personnalblock_nfo'] = 'Mostra blocos personalizados dentro do menu';
+$lang['g002_piwigo_default'] = 'Ordem padrão do Piwigo';
+$lang['g002_plugin'] = 'Plugin';
+$lang['g002_randompict'] = 'Foto Randômica';
+$lang['g002_randompict_nfo'] = 'Bloco de menus permite mostrar uma foto aleatória da galeria';
+$lang['g002_reset_order'] = 'Reinicia Ordem';
+$lang['g002_section'] = 'Bloco';
+$lang['g002_sectionid'] = 'Identificador';
+$lang['g002_sections'] = 'Blocos';
+$lang['g002_sectionslist'] = 'Bloco de Menus';
+$lang['g002_selected_all_gallery'] = 'Todos os älbuns';
+$lang['g002_selected_categories'] = 'Todos os álbuns selecionados';
+$lang['g002_selected_favorites_wm'] = 'Fotos favoritas do webmaster';
+$lang['g002_createoflink'] = 'Adicione link';
+$lang['g002_createofpersonalised'] = 'Cria um novo bloco personalizado';
+$lang['g002_createthelink'] = 'Adicione link';
+$lang['g002_editoflink'] = 'Edite link';
+$lang['g002_editofpersonalised'] = 'Modifique um bloco personalizado';
+$lang['g002_editthelink'] = 'Edite link';
+$lang['g002_error_invalid_ajax_call'] = 'Chamada de função inválida!';
+$lang['g002_icon'] = 'Foto representativa';
+$lang['g002_label'] = 'Rótulo';
+$lang['g002_labelmenu'] = 'Menu de Rótulo';
+$lang['g002_link'] = 'link';
+$lang['g002_links'] = 'links';
+$lang['g002_linkslist'] = 'Lista de links';
+$lang['g002_loading'] = 'Carregando...';
+$lang['g002_mode'] = 'Modo';
+$lang['g002_mode_current_window'] = 'Janela corrente';
+$lang['g002_mode_new_window'] = 'Nova janela';
+$lang['g002_modmenu'] = 'Bloco \'menu\'';
+$lang['g002_modspecial'] = 'Bloco \'Especial\'';
+$lang['g002_name'] = 'Rótulo';
+$lang['Grum Plugin Classes is not installed'] = 'Plugin <b>Grum Plugin Classes</b> nào está instalado';
+$lang['g002_accessibility'] = 'Acessibilidade';
+$lang['g002_addlink'] = 'Adiciona um link';
+$lang['g002_addlinks'] = 'Links';
+$lang['g002_addlinks_nfo'] = 'Bloco de menus permite dispor uma lista de endereços web';
+$lang['g002_addsection'] = 'Adiciona um novo bloco';
+$lang['g002_adviser_not_allowed'] = 'Aconselhável não permitir fazer mudanças';
+$lang['g002_album'] = 'Album &rarr;  menu';
+$lang['g002_album2menu'] = 'Convertido album para menu';
+$lang['g002_album_nfo'] = 'Permite converter um ou mais albuns dentro de menus';
+$lang['g002_all_languages'] = 'Todos os idiomas';
+$lang['g002_apply'] = 'Aplique';
+$lang['g002_apply_changes'] = 'Aplique mudanças';
+$lang['g002_cancel'] = 'Cancelar';
+$lang['g002_click_to_manage_rights'] = 'Gerencia direitos';
+$lang['g002_config_not_saved'] = 'Configuração não foi salva';
+$lang['g002_config_saved'] = 'Configuração  foi salva';
+$lang['g002_configlinks'] = 'Configuração de links';
+$lang['g002_configrandompic'] = 'Configuração de fotos randômicas';
+$lang['g002_confirm_delete_link'] = 'Apagar link ?';
+?>
