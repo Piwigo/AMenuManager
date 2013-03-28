c475bfb095c4565f2ebf5f2bdacba0c2b67f4fb3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-28 20:38:39 +0000

    [amenumanager] Add nn_NO, thanks to : kiho
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@21883 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nn_NO/plugin.lang.php
@@ -0,0 +1,105 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['g002_apply'] = 'Aktiver';
+$lang['g002_apply_changes'] = 'Aktiver endringar';
+$lang['g002_setting_albums_menus'] = 'Album som skal konverterast til meny';
+$lang['g002_setting_block_active'] = 'Vis blokk i meny';
+$lang['g002_setting_block_langchoice'] = 'Språkval';
+$lang['g002_setting_block_menu'] = 'Menyintegrasjon';
+$lang['g002_setting_block_title'] = 'Blokktittel';
+$lang['g002_setting_blocks_position'] = 'Synleghet & sortering for menyar';
+$lang['g002_setting_link_links'] = 'Lenker';
+$lang['g002_setting_link_show_icon'] = 'Vis representativt bilete for lenka';
+$lang['g002_setting_personalised_nfo'] = 'Skildring';
+$lang['g002_setting_randompic_showcomment'] = 'Vis biletkommentarar';
+$lang['g002_setting_randompic_showname'] = 'Vis biletnamn';
+$lang['g002_show_o'] = 'Vis over biletet';
+$lang['g002_show_u'] = 'Vis under biletet';
+$lang['g002_valid_order'] = 'Aktiver sortering';
+$lang['g002_setting_personalised_content'] = 'Innhald i blokken';
+$lang['g002_setting_personalised_properties'] = 'Eigenskapar';
+$lang['g002_setting_randompic_aboutpicture'] = 'Viste data';
+$lang['g002_setting_randompic_height'] = 'Høgde på menyblokk';
+$lang['g002_setting_randompic_height_auto'] = 'Automatisk';
+$lang['g002_show_n'] = 'Nei';
+$lang['g002_title'] = 'Tittel';
+$lang['g002_title_page'] = 'Advanced Menu Management';
+$lang['g002_url'] = 'URL';
+$lang['g002_version'] = 'r';
+$lang['g002_visible'] = 'Synleg';
+$lang['g002_yesno_n'] = 'Nei';
+$lang['g002_yesno_y'] = 'Ja';
+$lang['gmaps_gpc_not_up_to_date'] = 'Tilleggsprogrammet <i>Grum Plugin Classes</i> versjon %s må vera innstallera. Det er versjon %s som er innstallera nå. Du må oppgradera til nyare versjon av <i>Grum Plugin Classes</i>.';
+$lang['g002_plugin'] = 'Tilleggsprogram';
+$lang['g002_randompict'] = 'Tilfeldig bilete';
+$lang['g002_reset_order'] = 'Nullstill sortering';
+$lang['g002_section'] = 'blokk';
+$lang['g002_sections'] = 'blokkar';
+$lang['g002_selected_all_gallery'] = 'Alle album';
+$lang['g002_selected_categories'] = 'Alle valde album';
+$lang['g002_selected_favorites_wm'] = 'Webmaster sine favorittbilete';
+$lang['g002_selectedpict'] = 'Valde bilete';
+$lang['g002_setmenu'] = 'Menyhandtering';
+$lang['g002_mode_current_window'] = 'Gjeldande skjermbilete';
+$lang['g002_mode_new_window'] = 'Nytt skjermbilete';
+$lang['g002_modmenu'] = '\'meny\'-blokk';
+$lang['g002_name'] = 'Namn';
+$lang['g002_nolinks'] = 'Ingen lenker';
+$lang['g002_nosections'] = 'Ingen blokkar';
+$lang['g002_notitle'] = '[Ingen tittel]';
+$lang['g002_ok'] = 'OK';
+$lang['g002_owner'] = 'Eigar';
+$lang['g002_personalisedlist'] = 'List med personlege blokkar';
+$lang['g002_personnalblock'] = 'Tilpassa personled meny';
+$lang['g002_personnalblock_nfo'] = 'Vis personlege blokkar i menyen';
+$lang['g002_piwigo_default'] = 'Piwigo standardsortering';
+$lang['g002_createoflink'] = 'Legg til lenke';
+$lang['g002_createofpersonalised'] = 'Lag ein ny personleg blokk';
+$lang['g002_createthelink'] = 'Legg til lenke';
+$lang['g002_editoflink'] = 'Endre lenke';
+$lang['g002_editofpersonalised'] = 'Endre ein personleg blokk';
+$lang['g002_editthelink'] = 'Endre lenke';
+$lang['g002_error_invalid_ajax_call'] = 'Ugyldig funksjonskall!';
+$lang['g002_label'] = 'Namn';
+$lang['g002_labelmenu'] = 'Menynamn';
+$lang['g002_link'] = 'lenke';
+$lang['g002_links'] = 'lenker';
+$lang['g002_linkslist'] = 'Liste med lenker';
+$lang['g002_loading'] = 'Lastar opp...';
+$lang['g002_mode'] = 'Modus';
+$lang['Grum Plugin Classes is not installed'] = 'Tilleggsprogrammet <b>Grum Plugin Classes</b> er ikkje installera';
+$lang['g002_accessibility'] = 'Tilgangsrettar';
+$lang['g002_addlink'] = 'Legg til lenke';
+$lang['g002_addlinks'] = 'Lenker';
+$lang['g002_addsection'] = 'Legg til ein ny blokk';
+$lang['g002_adviser_not_allowed'] = 'Adviser har ikkje tilgang til å gjera endringar';
+$lang['g002_album'] = 'Album &rarr; meny';
+$lang['g002_album2menu'] = 'Konverter album til meny';
+$lang['g002_all_languages'] = 'Alle språk';
+$lang['g002_cancel'] = 'Abryt';
+$lang['g002_config_not_saved'] = 'Konfigurasjon vart ikkje lagra';
+$lang['g002_config_saved'] = 'Konfigurasjon vart lagra';
+$lang['g002_configlinks'] = 'Innstillingar for lenker';
+$lang['g002_configrandompic'] = 'Innstillingar for tilfeldig bilete';
+$lang['g002_confirm_delete_link'] = 'Fjern lenke?';
+?>
