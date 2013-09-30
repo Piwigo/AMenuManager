88125680aea16679d7780ad768f44b2bec62906b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-30 04:15:09 +0000

    [amenumanager] Add zh_TW, thanks to : azq1231
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@24694 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,119 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['g002_version'] = 'r';
+$lang['gmaps_gpc_not_up_to_date'] = '插件所必須的 <i> Grum類的插件 </i> 版本 %s 已安裝。
+現在版本 %s 已安裝：請進行插件的 <i> Grum類的插件 </i> 的版本升級。';
+$lang['g002_setting_randompic_periodicchange_delay'] = '延遲';
+$lang['g002_setting_randompic_showcomment'] = '顯示隨機圖片評論';
+$lang['g002_setting_randompic_showname'] = '顯示圖片擁有者';
+$lang['g002_show_n'] = '否';
+$lang['g002_show_o'] = '在隨機圖片上顯示';
+$lang['g002_show_u'] = '在隨機圖片下顯示';
+$lang['g002_title'] = '標題';
+$lang['g002_title_page'] = '高級菜單管理';
+$lang['g002_url'] = '連接地址';
+$lang['g002_valid_order'] = '應用排序';
+$lang['g002_visible'] = '可見';
+$lang['g002_yesno_n'] = '否';
+$lang['g002_yesno_y'] = '是';
+$lang['g002_selectedpict'] = '已選擇的圖片';
+$lang['g002_setmenu'] = '菜單管理';
+$lang['g002_setmenu_nfo'] = '菜單顯示管理';
+$lang['g002_setting_albums_menus'] = '相冊轉換成菜單';
+$lang['g002_setting_block_active'] = '是否顯示自定義模塊';
+$lang['g002_setting_block_langchoice'] = '選擇';
+$lang['g002_setting_block_menu'] = '菜單';
+$lang['g002_setting_block_title'] = '標題';
+$lang['g002_setting_blocks_position'] = '菜單的可見性和排序 ';
+$lang['g002_setting_core_blocks_content'] = '\'菜單\' 和\'特殊\' 菜單內容';
+$lang['g002_setting_link_links'] = '連接';
+$lang['g002_setting_link_show_icon'] = '是否顯示圖片的連接地址';
+$lang['g002_setting_personalised_content'] = '內容';
+$lang['g002_setting_personalised_nfo'] = '自定義模塊名稱';
+$lang['g002_setting_personalised_properties'] = '屬性';
+$lang['g002_setting_randompic_aboutpicture'] = '隨機圖片擁有者評論顯示設置';
+$lang['g002_setting_randompic_height'] = '隨機圖片模塊高度';
+$lang['g002_setting_randompic_height_auto'] = '自動';
+$lang['g002_setting_randompic_periodicchange'] = '定期改變形象';
+$lang['g002_setting_randompic_periodicchange_deactivated'] = '關閉';
+$lang['g002_nolinks'] = '沒有連接';
+$lang['g002_nosections'] = '無自定義模塊';
+$lang['g002_notitle'] = '[無標題]';
+$lang['g002_ok'] = '確定';
+$lang['g002_owner'] = '所有者';
+$lang['g002_personalisedlist'] = '自定義模塊';
+$lang['g002_personnalblock'] = '個性化菜單';
+$lang['g002_personnalblock_nfo'] = '添加編輯刪除';
+$lang['g002_piwigo_default'] = '默認排序';
+$lang['g002_plugin'] = '插件';
+$lang['g002_randompict'] = '隨機圖片';
+$lang['g002_randompict_nfo'] = '圖庫隨機圖片管理';
+$lang['g002_reset_order'] = '重置排序';
+$lang['g002_section'] = '塊';
+$lang['g002_sectionid'] = '標識符';
+$lang['g002_sections'] = '塊';
+$lang['g002_sectionslist'] = '菜單模塊';
+$lang['g002_selected_all_gallery'] = '全部相冊';
+$lang['g002_selected_categories'] = '全部選擇的相冊';
+$lang['g002_selected_favorites_wm'] = '站長的喜愛圖片';
+$lang['g002_createoflink'] = '添加連接';
+$lang['g002_createofpersonalised'] = '創建自定義模塊';
+$lang['g002_createthelink'] = '添加連接';
+$lang['g002_editoflink'] = '保存設置';
+$lang['g002_editofpersonalised'] = '修改自定義模塊';
+$lang['g002_editthelink'] = '保存設置';
+$lang['g002_error_invalid_ajax_call'] = '無效的函數調用';
+$lang['g002_icon'] = '代表圖片';
+$lang['g002_label'] = '網站名稱';
+$lang['g002_labelmenu'] = '菜單的標籤';
+$lang['g002_link'] = '連接';
+$lang['g002_links'] = '連接';
+$lang['g002_linkslist'] = '連接列表';
+$lang['g002_loading'] = '加載中...';
+$lang['g002_mode'] = '模式';
+$lang['g002_mode_current_window'] = '當前窗口';
+$lang['g002_mode_new_window'] = '新窗口';
+$lang['g002_modmenu'] = '\'菜單\'塊';
+$lang['g002_modspecial'] = '\'特殊\'塊';
+$lang['g002_name'] = '標籤';
+$lang['Grum Plugin Classes is not installed'] = '插件 <b>Grum Plugin Classes</b> 尚未安裝';
+$lang['g002_accessibility'] = '可訪問性';
+$lang['g002_addlink'] = '添加連接';
+$lang['g002_addlinks'] = '友情連接';
+$lang['g002_addlinks_nfo'] = '友情連接管理';
+$lang['g002_addsection'] = '添加新模塊';
+$lang['g002_adviser_not_allowed'] = '顧問是不允許進行更改';
+$lang['g002_album'] = '相冊⇒菜單';
+$lang['g002_album2menu'] = '已將相冊轉換成菜單';
+$lang['g002_album_nfo'] = '允許將一個或多個相冊轉換成菜單';
+$lang['g002_all_languages'] = '所有語言';
+$lang['g002_apply'] = '保存';
+$lang['g002_apply_changes'] = '應用更改';
+$lang['g002_cancel'] = '取消';
+$lang['g002_click_to_manage_rights'] = '設置權限';
+$lang['g002_config_not_saved'] = '配置不能保存';
+$lang['g002_config_saved'] = '配置已保存';
+$lang['g002_configlinks'] = '連接設置';
+$lang['g002_configrandompic'] = '隨機圖片設置';
+$lang['g002_confirm_delete_link'] = '確認刪除此項';
+?>
