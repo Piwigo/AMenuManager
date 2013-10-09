42dc283c64a305be72df344b44d6b934539eeb86
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-09 04:28:30 +0000

    [amenumanager] Update ar_SA, thanks to : aldambi
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@24813 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ar_SA/plugin.lang.php
+++ language/ar_SA/plugin.lang.php
@@ -2,20 +2,20 @@
 
 $lang['Grum Plugin Classes is not installed'] = 'الاداة <b>Grum Plugin Classes</b> غير مثبتة';
 
-$lang['g002_title_page'] = 'تنظيم القوائم الموسع';
+$lang['g002_title_page'] = 'ادارةالقوائم المتقدمة ';
 $lang['g002_version'] = 'اصدار';
 
-$lang['g002_error_invalid_ajax_call'] = "طريقة خاطئة في استدعاء الوظيفة";
+$lang['g002_error_invalid_ajax_call'] = 'دالة الاستدعاء غير صالحة!';
 
-$lang['g002_setmenu'] = 'تنظيم القائمة';
+$lang['g002_setmenu'] = 'ادارة القائمة';
 $lang['g002_addlinks'] = 'روابط';
 $lang['g002_randompict'] = 'صورة عشوائية';
 $lang['g002_personnalblock'] = 'قائمة مخصصة';
 
-$lang['g002_setmenu_nfo'] = 'تنظيم المناطق المعروضة';
-$lang['g002_addlinks_nfo'] = 'منطقة القوائم تسمح بعرض مجموعة من الروابط';
-$lang['g002_randompict_nfo'] = 'منطقة القوائم تسمح بعرض صورة عشوائية من المعرض';
-$lang['g002_personnalblock_nfo'] = 'عرض المناطق المخصصة في القائمة';
+$lang['g002_setmenu_nfo'] = 'ادارة بلوكات القائمة المعروضة';
+$lang['g002_addlinks_nfo'] = 'بلوك القائمة يسمح بعرض قائمة من الارتباطات التشعبيه';
+$lang['g002_randompict_nfo'] = 'بلوك القوائم يسمح بعرض صورة عشوائية من المعرض';
+$lang['g002_personnalblock_nfo'] = 'عرض البلوكات المخصصة في القائمة';
 
 
 $lang['g002_mode_new_window'] = 'نافذة جديدة';
@@ -23,12 +23,12 @@ $lang['g002_mode_current_window'] = 'النافذة الحالية';
 $lang['g002_addlink'] = 'اضافة رابط';
 $lang['g002_label'] = 'ملصق';
 $lang['g002_url'] = 'رابط';
-$lang['g002_mode'] = 'وضع';
-$lang['g002_icon'] = 'الصورة الممثلة';
+$lang['g002_mode'] = 'نمط';
+$lang['g002_icon'] = 'الصورة النموذجية';
 $lang['g002_visible'] = 'مرئي';
 $lang['g002_linkslist'] = "قائمة روابط";
 $lang['g002_confirm_delete_link'] = "حذف الرابط";
-$lang['g002_nolinks'] = 'لا روابط';
+$lang['g002_nolinks'] = 'لا توجد روابط';
 $lang['g002_link'] = 'رابط';
 $lang['g002_links'] = 'روابط';
 
@@ -39,79 +39,79 @@ $lang['g002_editthelink'] = 'تعديل رابط';
 
 $lang['g002_configlinks'] = 'اعدادات الروابط';
 $lang['g002_setting_link_links']='روابط';
-$lang['g002_setting_link_show_icon'] = 'عرض الصورة الممثلة للروابط';
+$lang['g002_setting_link_show_icon'] = 'عرض الصورة النموذجية للروابط';
 
-$lang['g002_setting_block_menu'] = 'دمج مع القائمة';
-$lang['g002_setting_block_active'] = 'عرض المنطقة في القائمة';
-$lang['g002_setting_block_title'] = 'عنوان المنطقة';
+$lang['g002_setting_block_menu'] = 'تكامل القائمة';
+$lang['g002_setting_block_active'] = 'عرض البلوك في القائمة';
+$lang['g002_setting_block_title'] = 'عنوان البلوك';
 $lang['g002_apply'] = 'تطبيق';
 
-$lang['g002_sectionslist'] = 'مناطق قائمة';
+$lang['g002_sectionslist'] = 'بلوكات القائمة';
 
 $lang['g002_yesno_y'] = 'نعم';
 $lang['g002_yesno_n'] = 'لا';
 
-$lang['g002_owner'] = 'مالك';
+$lang['g002_owner'] = 'المالك';
 $lang['g002_sectionid'] = 'المعرف';
-$lang['g002_name'] = 'لاصق';
+$lang['g002_name'] = 'ملصق';
 
 $lang['g002_configrandompic'] = 'اعدادات الصورة العشوائية';
-$lang['g002_setting_randompic_aboutpicture'] = 'المعلومات المعروضة';
+$lang['g002_setting_randompic_aboutpicture'] = 'البيانات المعروضة';
 $lang['g002_setting_randompic_showname'] = 'اظهر اسم الصورة';
-$lang['g002_setting_randompic_showcomment'] = 'اظهر تعليق الصورة';
+$lang['g002_setting_randompic_showcomment'] = 'اظهار تعليقات الصورة';
 $lang['g002_show_n'] = 'لا';
-$lang['g002_show_o'] = 'اظهر اعلى الصورة';
-$lang['g002_show_u'] = 'اظهر اسفل الصورة';
+$lang['g002_show_o'] = 'عرض اعلى الصورة';
+$lang['g002_show_u'] = 'عرض اسفل الصورة';
 
-$lang['g002_personalisedlist'] = "قائمة بالمناطق المخصصة";
+$lang['g002_personalisedlist'] = 'قائمة بابلوكات المخصصة';
 $lang['g002_notitle'] = 'لا عنوان';
 $lang['g002_title'] = 'العنوان';
-$lang['g002_sections'] = 'مناطق';
-$lang['g002_section'] = 'منطقة';
-$lang['g002_nosections'] = 'لا مناطق';
-$lang['g002_addsection'] = 'اضافة منطقة جديدة';
-$lang['g002_createofpersonalised'] = 'خلق منطقة جديدة مخصصة';
-$lang['g002_editofpersonalised'] = 'تعديل منطقة مخصصة';
+$lang['g002_sections'] = 'بلوكات';
+$lang['g002_section'] = 'بلوك';
+$lang['g002_nosections'] = 'لا توجد بلوكات';
+$lang['g002_addsection'] = 'اضافة بلوك جديد';
+$lang['g002_createofpersonalised'] = 'إنشاء بلوك شخصي جديد';
+$lang['g002_editofpersonalised'] = 'تعديل البلوك المخصص';
 $lang['g002_setting_block_langchoice'] = 'اختيار اللغة';
-$lang['g002_setting_personalised_content'] = 'محتوي المنطقة';
+$lang['g002_setting_personalised_content'] = 'محتوي البلوك';
 $lang['g002_setting_personalised_properties'] = 'خصائص';
 $lang['g002_setting_personalised_nfo'] = 'وصف';
 
 $lang['g002_all_languages'] = "كل اللغات";
 
-$lang['g002_modmenu'] = 'منطقة القائمة';
+$lang['g002_modmenu'] = 'بلوك القائمة';
 $lang['g002_modspecial'] = 'منطقة خاصة';
-$lang['g002_labelmenu'] = 'لاصق قائمة';
+$lang['g002_labelmenu'] = 'قائمة الملصق';
 
-$lang['g002_setting_randompic_periodicchange'] = "الفترة الدورية لتغيير الصورة";
+$lang['g002_setting_randompic_periodicchange'] = 'التغيير الدوري للصورة';
 $lang['g002_setting_randompic_periodicchange_delay'] = "تأخير";
 $lang['g002_setting_randompic_periodicchange_deactivated'] = "معطل";
-$lang['g002_setting_randompic_height'] = "ارتفاع منطقة القائمة";
+$lang['g002_setting_randompic_height'] = 'ارتفاع بلوك القائمة';
 $lang['g002_setting_randompic_height_auto'] = "تلقائي";
 
 // v2.1.3
 $lang['g002_cancel'] = "الغاء";
-$lang['g002_piwigo_default'] = "Piwigo الترتيب الافتراضي ل ";
+$lang['g002_piwigo_default'] = 'Piwigo الترتيب الافتراضي لـ ';
 $lang['g002_apply_changes'] = "تطبيق التغييرات";
-$lang['g002_config_saved'] = "الاعدادات حفظت";
+$lang['g002_config_saved'] = 'الاعدادات محفوظة';
 $lang['g002_adviser_not_allowed'] = "المراقب لايسمح له بعمل تغييرات";
-$lang['g002_click_to_manage_rights'] = "تنطيم الحقوق";
+$lang['g002_click_to_manage_rights'] = 'ادارة الحقوق';
 
 $lang['g002_accessibility'] = 'إمكانية الوصول';
 $lang['g002_album'] = 'ألبوم ⇒ القائمة';
 $lang['g002_album2menu'] = 'تحويل الألبوم إلى قائمة';
-$lang['g002_album_nfo'] = 'السمح لتحويل ألبوم أو أكثر إلى قائمة ';
-$lang['g002_config_not_saved'] = 'لم يتم حفظ التكوين الترتيب';
+$lang['g002_album_nfo'] = 'يسمح بتحويل البوم واحد او اكثر الى القائمة';
+$lang['g002_config_not_saved'] = 'لم يتم حفظ الاعدادات';
 $lang['g002_loading'] = '...تحميل';
-$lang['g002_ok'] = 'تمام';
-$lang['g002_plugin'] = 'البرنامج المساعد';
+$lang['g002_ok'] = 'موفق';
+$lang['g002_plugin'] = 'المكون الاضافي';
 $lang['g002_reset_order'] = 'إعادة الترتيب';
-$lang['g002_selected_all_gallery'] = 'جميع الألبومات';
-$lang['g002_selected_categories'] = 'جميع الألبومات المختارة';
-$lang['g002_selected_favorites_wm'] = 'أفضل صور الويبمستر';
-$lang['g002_selectedpict'] = 'الصور المختارة';
+$lang['g002_selected_all_gallery'] = 'كل الألبومات';
+$lang['g002_selected_categories'] = 'كل الألبومات المحددة';
+$lang['g002_selected_favorites_wm'] = 'صور مشرفي الموقع المفضلة';
+$lang['g002_selectedpict'] = 'الصور المحددة';
 $lang['g002_setting_albums_menus'] = 'الألبومات التي ستحول إلى قائمة';
 $lang['g002_setting_blocks_position'] = 'الرؤية وترتيب القوائم';
 $lang['g002_setting_core_blocks_content'] = 'محتوى \'القائمة\' والقوائم \'الخاصة\'';
-$lang['g002_valid_order'] = 'تطبيق النظام';
+$lang['g002_valid_order'] = 'تطبيق الترتيب';
 ?>
