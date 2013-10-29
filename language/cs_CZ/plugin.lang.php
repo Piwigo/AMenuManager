76ffcb2b7157ca109d3fd983d4a8b6ad0e1249b9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-29 12:55:11 +0000

    [amenumanager] Update cs_CZ, thanks to : elpresidento
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@25208 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/cs_CZ/plugin.lang.php
+++ language/cs_CZ/plugin.lang.php
@@ -5,14 +5,14 @@ $lang['Grum Plugin Classes is not installed'] = 'Plugin <b>Grum Plugin Classes</
 $lang['g002_title_page'] = 'Advanced Menu Management';
 $lang['g002_version'] = 'r';
 
-$lang['g002_error_invalid_ajax_call'] = "Invalid function call !";
+$lang['g002_error_invalid_ajax_call'] = 'Chyba volání funkce!';
 
-$lang['g002_setmenu'] = 'Menu management';
+$lang['g002_setmenu'] = 'Správa menu';
 $lang['g002_addlinks'] = 'Linky';
 $lang['g002_randompict'] = 'Nahodný obrázek';
 $lang['g002_personnalblock'] = 'Personalizované menu';
 
-$lang['g002_setmenu_nfo'] = 'Management pro zobrazení bloků menu';
+$lang['g002_setmenu_nfo'] = 'Správa zobrazení bloků menu';
 $lang['g002_addlinks_nfo'] = 'Menu blok povolující zobrazení seznamu hyperlinků';
 $lang['g002_randompict_nfo'] = 'Menu blok povolující zobrazení nahodného obrázku z galerie';
 $lang['g002_personnalblock_nfo'] = 'Zobrazí personalisované bloky v menu';
@@ -23,11 +23,11 @@ $lang['g002_mode_current_window'] = 'Stávající okno';
 $lang['g002_addlink'] = 'Přidat link';
 $lang['g002_label'] = 'Popis';
 $lang['g002_url'] = 'URL';
-$lang['g002_mode'] = 'Mode';
+$lang['g002_mode'] = 'Režim';
 $lang['g002_icon'] = 'Reprezentující obrázek';
 $lang['g002_visible'] = 'Viditelné';
 $lang['g002_linkslist'] = "Seznam odkazů";
-$lang['g002_confirm_delete_link'] = "Smazat odkaz ?";
+$lang['g002_confirm_delete_link'] = 'Smazat odkaz?';
 $lang['g002_nolinks'] = 'Žádné odkazy';
 $lang['g002_link'] = 'odkaz';
 $lang['g002_links'] = 'odkazy';
@@ -43,7 +43,7 @@ $lang['g002_setting_link_show_icon'] = 'Zobraz u odkazů reprezentující obráz
 
 $lang['g002_setting_block_menu'] = 'Integrace menu';
 $lang['g002_setting_block_active'] = 'Zobraz blok v menu';
-$lang['g002_setting_block_title'] = 'Blok nadpis';
+$lang['g002_setting_block_title'] = 'Nadpis bloku';
 $lang['g002_apply'] = 'Potvrď';
 
 $lang['g002_sectionslist'] = 'Menu bloky';
@@ -55,7 +55,7 @@ $lang['g002_owner'] = 'Vlastník';
 $lang['g002_sectionid'] = 'Identifikátor';
 $lang['g002_name'] = 'Popisek';
 
-$lang['g002_configrandompic'] = 'Náhodný obrázek nastaevní';
+$lang['g002_configrandompic'] = 'Nastavení náhodného obrázku';
 $lang['g002_setting_randompic_aboutpicture'] = 'Zobrazená data';
 $lang['g002_setting_randompic_showname'] = 'Ukaž název obrázku';
 $lang['g002_setting_randompic_showcomment'] = 'Ukaž komentář obrázku';
@@ -80,36 +80,36 @@ $lang['g002_setting_personalised_nfo'] = 'Popis';
 $lang['g002_all_languages'] = "Všechny jazyky";
 
 $lang['g002_modmenu'] = '\'menu\' blok';
-$lang['g002_modspecial'] = '\'special\' blok';
-$lang['g002_labelmenu'] = 'Menu název';
+$lang['g002_modspecial'] = '\'speciální\' blok';
+$lang['g002_labelmenu'] = 'Název menu';
 
 $lang['g002_setting_randompic_periodicchange'] = "Periodická změna obrázku";
-$lang['g002_setting_randompic_periodicchange_delay'] = "Zpoždění";
+$lang['g002_setting_randompic_periodicchange_delay'] = 'Prodleva';
 $lang['g002_setting_randompic_periodicchange_deactivated'] = "Vypnuto";
 $lang['g002_setting_randompic_height'] = "Výška bloků menu";
 $lang['g002_setting_randompic_height_auto'] = "Automat";
 
 // v2.1.3
 $lang['g002_cancel'] = "Zrušit";
-$lang['g002_piwigo_default'] = "Piwigo defaultní řazení";
+$lang['g002_piwigo_default'] = 'Výchozí řazení Piwigo';
 $lang['g002_apply_changes'] = "Aplikuj změny";
 $lang['g002_config_saved'] = "Konfigurace uložena";
 $lang['g002_adviser_not_allowed'] = "Není Vám dovoleno provádět změny";
-$lang['g002_click_to_manage_rights'] = "Práva managementu";
+$lang['g002_click_to_manage_rights'] = 'Správa oprávnění';
 
 
 // v3.0.0
 $lang['g002_ok'] = "OK";
 $lang['g002_config_saved'] = "Nastavení uloženo";
 $lang['g002_config_not_saved'] = "Nastavení nebylo uloženo";
-$lang['g002_selected_all_gallery'] = "Všechny alba";
+$lang['g002_selected_all_gallery'] = 'Všechna alba';
 $lang['g002_selected_favorites_wm'] = "Oblíbené fotky správce";
-$lang['g002_selected_categories'] = "Všechny vybraná alba";
+$lang['g002_selected_categories'] = 'Všechna vybraná alba';
 $lang['g002_loading'] = "Nahrávám...";
 $lang['g002_valid_order'] = "Použít";
 $lang['g002_reset_order'] = "Resetovat";
-$lang['g002_accessibility'] = "Přístupnost";
-$lang['g002_setting_core_blocks_content'] = "Obsah pro 'Menu' & 'Specials'";
+$lang['g002_accessibility'] = 'Dostupnost';
+$lang['g002_setting_core_blocks_content'] = 'Obsah pro \'Menu\' & \'Speciální\' menu';
 $lang['g002_setting_blocks_position'] = "Viditelnost & pořadí pro menu ";
 $lang['g002_plugin'] = "Plugin";
 $lang['g002_selectedpict'] = "Označené fotky";
