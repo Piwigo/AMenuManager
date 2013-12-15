c580298e93a945b2151b3c6ab5ed9f16806d8964
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-12-15 20:42:10 +0000

    [amenumanager] Update eo_EO, thanks to : sylvbarrier
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@25965 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/eo_EO/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['g002_createoflink'] = 'Aldoni ligilon';
+$lang['g002_createofpersonalised'] = 'Krei novan tajloritan blokon';
+$lang['g002_createthelink'] = 'Aldoni la ligilon';
+$lang['g002_editoflink'] = 'Redaktado de ligilo';
+$lang['g002_editofpersonalised'] = 'Modifi tajloritan blokon';
+$lang['g002_editthelink'] = 'Redakti la ligilon';
+$lang['g002_error_invalid_ajax_call'] = 'Nevalida voko de funkcio!';
+$lang['g002_link'] = 'ligilo';
+$lang['g002_links'] = 'ligiloj';
+$lang['g002_mode'] = 'Modo';
+$lang['Grum Plugin Classes is not installed'] = 'La kromaĵo <b>Grum Plugin Classes</b> ne estas instalita.';
+$lang['g002_accessibility'] = 'Alirebleco';
+$lang['g002_addlink'] = 'Aldoni ligilon';
+$lang['g002_addlinks'] = 'Ligiloj';
+$lang['g002_addlinks_nfo'] = 'Menubloko permesanta vidigi liston de eksteraj ligiloj';
+$lang['g002_addsection'] = 'Aldoni novan blokon';
+$lang['g002_adviser_not_allowed'] = 'Konsilanto ne rajtas fari ŝanĝojn';
+$lang['g002_album'] = 'Fotaro &rArr; menuo';
+$lang['g002_album2menu'] = 'Konverti fotaron al menuo';
+$lang['g002_album_nfo'] = 'Permesi konverti unu aŭ pli da fotaroj al menuo';
+$lang['g002_all_languages'] = 'Ĉiuj lingvoj';
+$lang['g002_apply'] = 'Apliki';
+$lang['g002_apply_changes'] = 'Apliki la ŝanĝojn';
+$lang['g002_cancel'] = 'Nuligi';
+$lang['g002_click_to_manage_rights'] = 'Administrado de rajtoj';
+$lang['g002_config_not_saved'] = 'Agordo ne konserviĝis';
+$lang['g002_config_saved'] = 'Agordo konserviĝis';
+$lang['g002_configlinks'] = 'Agordoj de ligiloj';
+$lang['g002_configrandompic'] = 'Agordoj de hazardaj ligiloj';
+$lang['g002_confirm_delete_link'] = 'Ĉu forigi ligilon?';
+?>
