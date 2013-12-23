e6c35a6b39241eef893d8d7cb6e151e3e695f5c8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-12-23 13:05:45 +0000

    [amenumanager] Add bg_BG, thanks to : Martian
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@26120 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,94 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['g002_setting_personalised_nfo'] = 'Описание';
+$lang['g002_setting_randompic_height_auto'] = 'Автоматично';
+$lang['g002_show_n'] = 'Не';
+$lang['g002_title'] = 'Заглавие';
+$lang['g002_sections'] = 'блокове';
+$lang['g002_selected_all_gallery'] = 'Всички албуми';
+$lang['g002_selected_categories'] = 'Всички избрани албуми';
+$lang['g002_selected_favorites_wm'] = 'Любимите снимки на Уебмастъра';
+$lang['g002_selectedpict'] = 'Изпраните снимки';
+$lang['g002_setmenu'] = 'Управление на менюто';
+$lang['g002_setmenu_nfo'] = 'Управление на показаните меню блокове';
+$lang['g002_setting_albums_menus'] = 'Албуми за преобразуване в меню';
+$lang['g002_setting_block_active'] = 'Покажи блока в менюто';
+$lang['g002_setting_block_langchoice'] = 'Избери език';
+$lang['g002_setting_block_menu'] = 'Интеграция в менюто';
+$lang['g002_setting_block_title'] = 'Заглавие на блока';
+$lang['g002_setting_blocks_position'] = 'Показване & подредба на менютата';
+$lang['g002_setting_core_blocks_content'] = 'Съдържание на \'Меню\' & \'Специални\' менюта';
+$lang['g002_setting_link_links'] = 'Връзки';
+$lang['g002_modspecial'] = '\'специален\' блок';
+$lang['g002_name'] = 'Надпис';
+$lang['g002_nolinks'] = 'Няма връзки';
+$lang['g002_nosections'] = 'Няма блокове';
+$lang['g002_notitle'] = '[Няма заглавие]';
+$lang['g002_ok'] = 'ОК';
+$lang['g002_owner'] = 'Собственик';
+$lang['g002_personalisedlist'] = 'Списък с персонализираните блокове';
+$lang['g002_personnalblock'] = 'Персонализирано меню';
+$lang['g002_personnalblock_nfo'] = 'Покажи личните блокове в меню';
+$lang['g002_piwigo_default'] = 'Piwigo стандартно сортиране';
+$lang['g002_plugin'] = 'Добавка';
+$lang['g002_randompict'] = 'Произволна снимка';
+$lang['g002_randompict_nfo'] = 'Меню блок показващ произволна снимка от галерията';
+$lang['g002_reset_order'] = 'Нулирай подредбата';
+$lang['g002_section'] = 'блок';
+$lang['g002_sectionid'] = 'Идентификатор';
+$lang['g002_createoflink'] = 'Добави връзка';
+$lang['g002_createofpersonalised'] = 'Създай нов личен блок';
+$lang['g002_createthelink'] = 'Добави връзка';
+$lang['g002_editoflink'] = 'Редактирай връзка';
+$lang['g002_editofpersonalised'] = 'Редактирай личен блок';
+$lang['g002_editthelink'] = 'Редактирай връзка';
+$lang['g002_icon'] = 'Основна снимка';
+$lang['g002_label'] = 'Заглавие';
+$lang['g002_labelmenu'] = 'Заглавие на менюто';
+$lang['g002_link'] = 'връзка';
+$lang['g002_links'] = 'връзки';
+$lang['g002_linkslist'] = 'Списък с връзките';
+$lang['g002_loading'] = 'Зареждане...';
+$lang['g002_mode'] = 'Режим';
+$lang['g002_mode_current_window'] = 'Текущ прозорец';
+$lang['g002_mode_new_window'] = 'Нов прозорец';
+$lang['g002_modmenu'] = '\'меню\' блок';
+$lang['Grum Plugin Classes is not installed'] = 'Плъгин <b>Grum Plugin Classes</b> не е инсталиран';
+$lang['g002_accessibility'] = 'Достъпност';
+$lang['g002_addlink'] = 'Добави връзка';
+$lang['g002_addlinks'] = 'Връзки';
+$lang['g002_addlinks_nfo'] = 'Меню блок показваш списък с препратки';
+$lang['g002_addsection'] = 'Добави нов блок';
+$lang['g002_album2menu'] = 'Меню от албум';
+$lang['g002_album_nfo'] = 'Позволява преобразуването на един или повече албуми в меню';
+$lang['g002_all_languages'] = 'Всички езици';
+$lang['g002_apply'] = 'Приложи';
+$lang['g002_apply_changes'] = 'Приложи настройките';
+$lang['g002_cancel'] = 'Откажи';
+$lang['g002_click_to_manage_rights'] = 'Управление на правата';
+$lang['g002_config_not_saved'] = 'Настройката не беше записана';
+$lang['g002_config_saved'] = 'Настройката е записана';
+$lang['g002_configlinks'] = 'Настройка на връзките';
+$lang['g002_configrandompic'] = 'Настройки за произволни снимки';
+$lang['g002_confirm_delete_link'] = 'Изтрий връзката?';
+?>
