666c0b118d479aa852689e303d78d2a5ac37d620
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-14 20:23:01 +0000

    [amenumanager] Update fa_IR, thanks to : ipic
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@26705 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fa_IR/plugin.lang.php
@@ -0,0 +1,129 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['g002_setting_link_show_icon'] = '
+نمایش لینک ها در تصاویر نمایندگان';
+$lang['g002_show_o'] = 'نمایش روی عکس';
+$lang['g002_title_page'] = 'مدیریت نو های سفارشی';
+$lang['gmaps_gpc_not_up_to_date'] = 'لازم است برای پلاگین که <i> کلاس Grum پلاگین < i/> نسخه%s نصب شده است.
+در حال حاضر، نسخه%s نصب شده است: لطفا با نسخه به روز رسانی از پلاگین <i> کلاس Grum پلاگین </i> است ادامه دهید.';
+$lang['g002_setmenu_nfo'] = 'مدیریت نمایش تصاویر';
+$lang['g002_setting_albums_menus'] = 'البوم  تبدل به منو شود';
+$lang['g002_setting_block_active'] = 'نمایش بلوک در منو';
+$lang['g002_setting_block_langchoice'] = 'انتخاب زبان';
+$lang['g002_setting_block_menu'] = 'یک پارچه سازی منو ها';
+$lang['g002_setting_block_title'] = 'عنوان بلوک';
+$lang['g002_setting_blocks_position'] = '
+قابلیت دید و مرتب سازی برای منوها';
+$lang['g002_setting_core_blocks_content'] = '
+محتوا برای \'منو\' و منوها \'ویژه\'';
+$lang['g002_setting_randompic_aboutpicture'] = 'نمایش تاریخ';
+$lang['g002_setting_randompic_height'] = 'بلندای بلوک منو';
+$lang['g002_setting_randompic_periodicchange'] = 'تغییر دوره ای تصویر';
+$lang['g002_setting_randompic_periodicchange_delay'] = 'تاخیر';
+$lang['g002_setting_randompic_showcomment'] = 'نمایش دیگاه های تصاویر';
+$lang['g002_setting_randompic_showname'] = 'نمایش نام تصاویر';
+$lang['g002_show_n'] = 'نه';
+$lang['g002_show_u'] = '
+نمایش دادن زیر عکس';
+$lang['g002_title'] = 'عنوان';
+$lang['g002_url'] = 'پیوند';
+$lang['g002_valid_order'] = 'مرتب سازی انجام شد';
+$lang['g002_version'] = 'r';
+$lang['g002_visible'] = 'مقدار';
+$lang['g002_yesno_n'] = 'نه';
+$lang['g002_yesno_y'] = 'بله';
+$lang['g002_createofpersonalised'] = 'ساختن یک بلوک شخصی';
+$lang['g002_editofpersonalised'] = '
+اصلاح یک بلوک شخصی';
+$lang['g002_error_invalid_ajax_call'] = '
+فراخوانی تابع نامعتبر است!';
+$lang['g002_icon'] = '
+تصاویر نمایندگان';
+$lang['g002_randompict_nfo'] = 'منوی بلوک اجازه می دهد برای نمایش تصویر تصادفی از گالری';
+$lang['g002_sectionslist'] = 'منوی بلوک';
+$lang['g002_selected_all_gallery'] = 'تما البوم ها';
+$lang['g002_selected_categories'] = 'تمام البوم ها انتخاب شود';
+$lang['g002_selected_favorites_wm'] = 'این تصاویر را مدیر می پسندد';
+$lang['g002_selectedpict'] = 'تصاویرانتخاب شده';
+$lang['g002_setmenu'] = 'مدیریت منو ها';
+$lang['g002_setting_link_links'] = 'لینکها';
+$lang['g002_setting_personalised_content'] = 'بلوکهای محتوا';
+$lang['g002_setting_personalised_nfo'] = 'توصیف';
+$lang['g002_setting_personalised_properties'] = 'خواصیت ها';
+$lang['g002_setting_randompic_height_auto'] = 'خودکار';
+$lang['g002_setting_randompic_periodicchange_deactivated'] = 'غیر فعال';
+$lang['g002_link'] = 'لینک';
+$lang['g002_links'] = 'لینکها';
+$lang['g002_linkslist'] = 'اخری لینک ها';
+$lang['g002_loading'] = 'درحال بارگذاری....';
+$lang['g002_mode'] = 'مدل';
+$lang['g002_mode_current_window'] = 'پنجره جاری';
+$lang['g002_mode_new_window'] = 'پنجره جدید';
+$lang['g002_modmenu'] = ' بلوک \'منو\' ';
+$lang['g002_modspecial'] = '\'ویژه\' بلوک';
+$lang['g002_name'] = 'برچسب';
+$lang['g002_nolinks'] = 'بدون لینک';
+$lang['g002_nosections'] = 'بدون بلوک';
+$lang['g002_notitle'] = '[بدون عنوان]';
+$lang['g002_ok'] = 'خوبه';
+$lang['g002_owner'] = 'شخصی';
+$lang['g002_personalisedlist'] = '
+فهرستی از بلوک های شخصی';
+$lang['g002_personnalblock'] = 'منوی شخصی';
+$lang['g002_personnalblock_nfo'] = 'نمایش بلوک های شخصی در داخل منو';
+$lang['g002_piwigo_default'] = '
+Piwigo مرتب سازی بر پیش فرض';
+$lang['g002_plugin'] = 'افزونه ';
+$lang['g002_randompict'] = 'تصویر اتفاقی';
+$lang['g002_reset_order'] = 'باز نشانی مرتب سازی';
+$lang['g002_section'] = 'بلوک';
+$lang['g002_sectionid'] = 'تعیین هویت';
+$lang['g002_sections'] = 'بلوکها';
+$lang['Grum Plugin Classes is not installed'] = 'پلاگین <b>Grum Plugin Classes</b>نصب نیست ';
+$lang['g002_accessibility'] = 'اختیارها';
+$lang['g002_addlink'] = 'افزود لینک';
+$lang['g002_addlinks'] = 'لینک ها';
+$lang['g002_addlinks_nfo'] = 'منوی بلوک  برای نمایش یک لیست از لینک اجازه می دهد';
+$lang['g002_addsection'] = 'افزودن یک بلوک';
+$lang['g002_adviser_not_allowed'] = '
+مشاور مجاز به انجام تغییرات نیست';
+$lang['g002_album'] = 'البوم  &rArr;منو';
+$lang['g002_album2menu'] = 'آلبوم تبدیل به منوی';
+$lang['g002_album_nfo'] = ' برای تبدیل یک یا چند آلبوم را در یک منواجازه می دهد';
+$lang['g002_all_languages'] = 'تمام زبان ها';
+$lang['g002_apply'] = 'انجام شود';
+$lang['g002_apply_changes'] = 'تغییرات انجام شد';
+$lang['g002_cancel'] = 'لغو';
+$lang['g002_click_to_manage_rights'] = 'مدیریت حقوق';
+$lang['g002_config_not_saved'] = 'نمی توان پیکر بندی را ذخیره کرد';
+$lang['g002_config_saved'] = 'پکربندی ذخیره شد';
+$lang['g002_configlinks'] = 'تنظیات لینک';
+$lang['g002_configrandompic'] = 'تنظیمات تصویر تصادفی';
+$lang['g002_confirm_delete_link'] = 'لینک حذف شود؟ ';
+$lang['g002_createoflink'] = 'افزودن لینک';
+$lang['g002_createthelink'] = 'افزودن لینک';
+$lang['g002_editoflink'] = 'ویرایش لینک';
+$lang['g002_editthelink'] = 'ویرایش لینک';
+$lang['g002_label'] = 'برچسب';
+$lang['g002_labelmenu'] = 'برچسب منوها';
+?>
