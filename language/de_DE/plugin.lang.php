24fd0908591699a0cdb729f7bb1670958d77a3da
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-11-25 06:03:20 +0000

    [amenumanager] Update de_DE, thanks to bigant
    git-svn-id: http://piwigo.org/svn/extensions/AMenuManager@30596 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/de_DE/plugin.lang.php
+++ language/de_DE/plugin.lang.php
@@ -2,92 +2,92 @@
 
 $lang['Grum Plugin Classes is not installed'] = 'Das Plugin <b>Grum Plugin Klassen</b> ist nicht installiert';
 
-$lang['g002_title_page'] = 'Advanced Menu Management';
+$lang['g002_title_page'] = 'Erweiterte Menüverwaltung';
 $lang['g002_version'] = 'r';
 
-$lang['g002_error_invalid_ajax_call'] = "Ungültige Funktion Anruf!";
+$lang['g002_error_invalid_ajax_call'] = 'Ungültiger Funktionsaufruf!';
 
-$lang['g002_setmenu'] = 'Menü-Management';
-$lang['g002_addlinks'] = 'Links';
+$lang['g002_setmenu'] = 'Menü-Verwaltung';
+$lang['g002_addlinks'] = 'Verweise';
 $lang['g002_randompict'] = 'Zufallsbild';
-$lang['g002_personnalblock'] = 'Personalisierte Menü';
+$lang['g002_personnalblock'] = 'Personalisiertes Menü';
 
-$lang['g002_setmenu_nfo'] = 'Management der angezeigten Menü Blöcke';
-$lang['g002_addlinks_nfo'] = 'Abschnitt für die Anzeige in dem Menü eine Liste mit Links zu anderen Websites';
-$lang['g002_randompict_nfo'] = 'Abschnitt für die Anzeige in dem Menü einer Aufnahme zufällig in der Galerie';
-$lang['g002_personnalblock_nfo'] = 'Anzeige Personalisierte in Blöcke Menü';
+$lang['g002_setmenu_nfo'] = 'Verwaltung der angezeigten Menübereiche';
+$lang['g002_addlinks_nfo'] = 'Menü-Abschnitt erlauben, eine Liste mit Hyperlinks anzuzeigen';
+$lang['g002_randompict_nfo'] = 'Menüeintrag erlauben, ein Zufallsbild der Galerie anzuzeigen';
+$lang['g002_personnalblock_nfo'] = 'Personalisierte Bereiche im Menü anzeigen';
 
 
 $lang['g002_mode_new_window'] = 'Neues Fenster';
-$lang['g002_mode_current_window'] = 'Aktuelle Fenster';
-$lang['g002_addlink'] = 'Link hinzufügen';
-$lang['g002_label'] = 'Wortlaut';
+$lang['g002_mode_current_window'] = 'Aktuelles Fenster';
+$lang['g002_addlink'] = 'Verweis hinzufügen';
+$lang['g002_label'] = 'Etikett';
 $lang['g002_url'] = 'URL';
-$lang['g002_mode'] = 'Mode';
+$lang['g002_mode'] = 'Modus';
 $lang['g002_icon'] = 'Repräsentatives Bild';
-$lang['g002_visible'] = 'Sichtbare';
-$lang['g002_linkslist'] = "Links Liste";
-$lang['g002_confirm_delete_link'] = "Link Löschen?";
-$lang['g002_nolinks'] = 'Keine Links';
-$lang['g002_link'] = 'Link';
-$lang['g002_links'] = 'Links';
+$lang['g002_visible'] = 'Sichtbar';
+$lang['g002_linkslist'] = 'Liste der Verweise';
+$lang['g002_confirm_delete_link'] = 'Verweis löschen?';
+$lang['g002_nolinks'] = 'Keine Verweise';
+$lang['g002_link'] = 'Verweis';
+$lang['g002_links'] = 'Verweise';
 
-$lang['g002_createoflink'] = 'Link hinzufügen';
-$lang['g002_editoflink'] = 'Link Bearbeiten';
-$lang['g002_createthelink'] = 'Link hinzufügen';
-$lang['g002_editthelink'] = 'Link Bearbeiten';
+$lang['g002_createoflink'] = 'Verweis hinzufügen';
+$lang['g002_editoflink'] = 'Verweis bearbeiten';
+$lang['g002_createthelink'] = 'Verweis hinzufügen';
+$lang['g002_editthelink'] = 'Verweis bearbeiten';
 
-$lang['g002_configlinks'] = 'Links Einstellungen';
-$lang['g002_setting_link_links']='Links';
-$lang['g002_setting_link_show_icon'] = 'Anzeige Links repräsentatives Bild';
+$lang['g002_configlinks'] = 'Verweis-Einstellungen';
+$lang['g002_setting_link_links'] = 'Verweise';
+$lang['g002_setting_link_show_icon'] = 'Anzeige desrepräsentativen Bildes der Verweise';
 
-$lang['g002_setting_block_menu'] = 'Menü Integration';
-$lang['g002_setting_block_active'] = 'Anzeigen der Abschnitt in dem Menü';
-$lang['g002_setting_block_title'] = 'Titel des Abschnitts in dem Menü';
+$lang['g002_setting_block_menu'] = 'Menü-Integration';
+$lang['g002_setting_block_active'] = 'Bereich in Menü anzeigen';
+$lang['g002_setting_block_title'] = 'Titel des Bereichs';
 $lang['g002_apply'] = 'Anwenden';
 
-$lang['g002_sectionslist'] = 'Menü-Abschnitte';
+$lang['g002_sectionslist'] = 'Menü-Breiche';
 
 $lang['g002_yesno_y'] = 'Ja';
 $lang['g002_yesno_n'] = 'Nein';
 
-$lang['g002_owner'] = 'Eigentümer';
+$lang['g002_owner'] = 'Besitzer';
 $lang['g002_sectionid'] = 'Benutzername';
-$lang['g002_name'] = 'Wortlaut';
+$lang['g002_name'] = 'Etikett';
 
-$lang['g002_configrandompic'] = 'Konfiguration des Zufallsbild';
-$lang['g002_setting_randompic_aboutpicture'] = 'Angezeigt Daten';
-$lang['g002_setting_randompic_showname'] = 'Anzuzeigen des Bildes Namen';
-$lang['g002_setting_randompic_showcomment'] = 'Bild Kommentar ansehen';
+$lang['g002_configrandompic'] = 'Konfiguration des Zufallsbildes';
+$lang['g002_setting_randompic_aboutpicture'] = 'Angezeigte Daten';
+$lang['g002_setting_randompic_showname'] = 'Bildnamen anzeigen';
+$lang['g002_setting_randompic_showcomment'] = 'Bildkommentar anzeigen';
 $lang['g002_show_n'] = 'Nein';
-$lang['g002_show_o'] = 'Anzeige über das Bild';
-$lang['g002_show_u'] = 'Anzeige unter das Bild';
+$lang['g002_show_o'] = 'Anzeige oberhalb des Bildes';
+$lang['g002_show_u'] = 'Anzeige unterhalb des Bildes';
 
-$lang['g002_personalisedlist'] = "Liste der personalisierten Abschnitte";
+$lang['g002_personalisedlist'] = 'Liste der personalisierten Bereiche';
 $lang['g002_notitle'] = '[Ohne Titel]';
 $lang['g002_title'] = 'Titel';
-$lang['g002_sections'] = 'Abschnitte';
-$lang['g002_section'] = 'Abschnitt';
-$lang['g002_nosections'] = 'Kein Abschnitte';
-$lang['g002_addsection'] = 'Hinzufügen neue Abschnitt';
-$lang['g002_createofpersonalised'] = 'Erstellen eine neue personalisierte Abschnitt';
-$lang['g002_editofpersonalised'] = 'Ändern eine personalisierte Abschnitt';
+$lang['g002_sections'] = 'Bereiche';
+$lang['g002_section'] = 'Bereich';
+$lang['g002_nosections'] = 'Kein Bereiche';
+$lang['g002_addsection'] = 'Hinzufügen eines neuen Abschnittes';
+$lang['g002_createofpersonalised'] = 'Erstellen eines neuen personalisierten Bereiches';
+$lang['g002_editofpersonalised'] = 'Ändern eines personalisierten Bereiches';
 $lang['g002_setting_block_langchoice'] = 'Sprachwahl';
-$lang['g002_setting_personalised_content'] = 'Inhalt des Abschnitts';
+$lang['g002_setting_personalised_content'] = 'Inhalt des Bereichs';
 $lang['g002_setting_personalised_properties'] = 'Eigenschaften';
 $lang['g002_setting_personalised_nfo'] = 'Beschreibung';
 
 $lang['g002_all_languages'] = "Alle Sprachen";
 
-$lang['g002_modmenu'] = '\'Menü\' Abschnitt';
-$lang['g002_modspecial'] = '\'speciale\' Abschnitt';
-$lang['g002_labelmenu'] = 'Menü Wortlaut';
+$lang['g002_modmenu'] = '\'Menü\'-Bereich';
+$lang['g002_modspecial'] = '\'Spezial\'-Bereich';
+$lang['g002_labelmenu'] = 'Menü-Etikett';
 
 
 $lang['g002_setting_randompic_periodicchange'] = "Periodische Änderung des Bildes";
-$lang['g002_setting_randompic_periodicchange_delay'] = "Frist";
+$lang['g002_setting_randompic_periodicchange_delay'] = 'Dauer';
 $lang['g002_setting_randompic_periodicchange_deactivated'] = "Deaktiviert";
-$lang['g002_setting_randompic_height'] = "Höhe der Block Menü";
+$lang['g002_setting_randompic_height'] = 'Höhe des Menübereiches';
 $lang['g002_setting_randompic_height_auto'] = "Auto";
 
 $lang['g002_ok'] = "OK";
@@ -97,24 +97,23 @@ $lang['g002_selected_all_gallery'] = "Alle Alben";
 $lang['g002_selected_favorites_wm'] = "Die Favoriten des Webmasters";
 $lang['g002_selected_categories'] = "Alle ausgewählten Alben";
 $lang['g002_loading'] = "Laden...";
-$lang['g002_valid_order'] = "Befehl anwenden";
-$lang['g002_reset_order'] = "Befehl zurücksetzen";
-$lang['g002_accessibility'] = "Zugänglichkeit";
-$lang['g002_setting_core_blocks_content'] = "Inhalt der Menüs 'Menu' & 'Specials'";
-$lang['g002_setting_blocks_position'] = "Sichtbarkeit und Reihenfolge für die Menüs ";
+$lang['g002_valid_order'] = 'Befehl ausführen';
+$lang['g002_reset_order'] = 'Reihenfolge zurücksetzen';
+$lang['g002_accessibility'] = 'Barrierefreiheit';
+$lang['g002_setting_core_blocks_content'] = 'Inhalt der Menüs \'Menu\' & \'Spezial\'';
+$lang['g002_setting_blocks_position'] = 'Sichtbarkeit und Reihenfolge der Menüs';
 $lang['g002_plugin'] = "Plug-In";
 $lang['g002_selectedpict'] = "Ausgewählte Bilder";
-$lang['g002_album_nfo'] = 'Die Konvertierung eines oder mehrere Alben in einem Menü erlauben';
-$lang['g002_album2menu'] = "Konvertierter Album zum Menü";
+$lang['g002_album_nfo'] = 'Die Konvertierung eines oder mehrerer Alben in einem Menü erlauben';
+$lang['g002_album2menu'] = 'Album in Menü umgewandelt';
 $lang['g002_album'] = "Album &rArr; Menü";
-$lang['g002_setting_albums_menus'] = "Zu konvertierenden Alben in Menüs.";
-$lang['gmaps_gpc_not_up_to_date']="Es ist für den Plug-In notwendig, dass <i>Grum Plugin Classes</i> mit der Version %s installiert ist.
-Zurzeit ist die Version %s installiert: bitte fahren Sie mit der Updateversion des Plug-Ins <i>Grum Plugin Classes</i> fort.";
+$lang['g002_setting_albums_menus'] = 'Umzuwandelnde Alben zu Menü';
+$lang['gmaps_gpc_not_up_to_date'] = 'Es ist für das Plug-In notwendig, dass <i>Grum Plugin Classes</i> mit der Version %s installiert ist.
+Zurzeit ist die Version %s installiert: bitte fahren Sie mit der aktualisierten Version des Plug-Ins <i>Grum Plugin Classes</i> fort.';
 
 
-$lang['g002_adviser_not_allowed'] = 'Berater ist nicht erlaubt um Änderungen vorzunehmen';
+$lang['g002_adviser_not_allowed'] = 'Betreuer können keine Änderungen vornehmen';
 $lang['g002_apply_changes'] = 'Änderungen übernehmen';
-$lang['g002_cancel'] = 'Zurücknehmen';
-$lang['g002_click_to_manage_rights'] = 'Rechten Verwaltung';
+$lang['g002_cancel'] = 'Abbrechen';
+$lang['g002_click_to_manage_rights'] = 'Rechteverwaltung';
 $lang['g002_piwigo_default'] = 'Piwigo Standardsortierreihenfolge';
-?>
