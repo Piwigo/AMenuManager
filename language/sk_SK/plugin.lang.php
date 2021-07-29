c8f903113c84534ec2eaf3655618832a0efde6c7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-07-29 10:13:23 +0200

    Update sk_SK, thanks to jose1711

--- language/sk_SK/plugin.lang.php
+++ language/sk_SK/plugin.lang.php
@@ -8,40 +8,40 @@ $lang['g002_version'] = 'r';
 $lang['g002_error_invalid_ajax_call'] = "Volaná neplatná funkcia!";
 
 $lang['g002_setmenu'] = 'Správa menu';
-$lang['g002_addlinks'] = 'Linky';
-$lang['g002_randompict'] = 'Náhodné fotografie';
+$lang['g002_addlinks'] = 'Odkaz';
+$lang['g002_randompict'] = 'Náhodná fotografia';
 $lang['g002_personnalblock'] = 'Individuálne menu';
 
-$lang['g002_setmenu_nfo'] = 'Správa zobraných blokov menu';
-$lang['g002_addlinks_nfo'] = 'Blok menu povoľujúci zobraziť zoznam hyperliniek';
+$lang['g002_setmenu_nfo'] = 'Správa zobrazených blokov menu';
+$lang['g002_addlinks_nfo'] = 'Blok menu povoľujúci zobraziť zoznam odkazov';
 $lang['g002_randompict_nfo'] = 'Blok menu povoľujúci zobraziť náhodný obrázok z galérie';
 $lang['g002_personnalblock_nfo'] = 'Zobrazenie individuálnych blokov v menu';
 
 
 $lang['g002_mode_new_window'] = 'Nové okno';
 $lang['g002_mode_current_window'] = 'Aktuálne okno';
-$lang['g002_addlink'] = 'Pridať link';
+$lang['g002_addlink'] = 'Pridať odkaz';
 $lang['g002_label'] = 'Označenie';
 $lang['g002_url'] = 'URL';
-$lang['g002_mode'] = 'Mód';
+$lang['g002_mode'] = 'Režim';
 $lang['g002_icon'] = 'Reprezentatívna fotka';
 $lang['g002_visible'] = 'Viditeľné';
-$lang['g002_linkslist'] = "Zoznam liniek";
-$lang['g002_confirm_delete_link'] = "Vymazať link ?";
-$lang['g002_nolinks'] = 'Žiadne linky';
-$lang['g002_link'] = 'link';
-$lang['g002_links'] = 'linky';
+$lang['g002_linkslist'] = 'Zoznam odkazov';
+$lang['g002_confirm_delete_link'] = 'Odstrániť odkaz?';
+$lang['g002_nolinks'] = 'Žiadne odkazy';
+$lang['g002_link'] = 'odkaz';
+$lang['g002_links'] = 'odkazy';
 
-$lang['g002_createoflink'] = 'Pridať link';
-$lang['g002_editoflink'] = 'Upraviť link';
-$lang['g002_createthelink'] = 'Pridať link';
-$lang['g002_editthelink'] = 'Upraviť link';
+$lang['g002_createoflink'] = 'Pridať odkaz';
+$lang['g002_editoflink'] = 'Upraviť odkaz';
+$lang['g002_createthelink'] = 'Pridať odkaz';
+$lang['g002_editthelink'] = 'Upraviť odkaz';
 
-$lang['g002_configlinks'] = 'Nastavenie liniek';
-$lang['g002_setting_link_links']='Linky';
-$lang['g002_setting_link_show_icon'] = 'Zobraziť linky reprezentatívnej fotografie';
+$lang['g002_configlinks'] = 'Nastavenie odkazov';
+$lang['g002_setting_link_links'] = 'Odkazy';
+$lang['g002_setting_link_show_icon'] = 'Zobraziť odkazy reprezentatívnej fotografie';
 
-$lang['g002_setting_block_menu'] = 'Spojenie menu';
+$lang['g002_setting_block_menu'] = 'Integrácia menu';
 $lang['g002_setting_block_active'] = 'Zobraziť blok v menu';
 $lang['g002_setting_block_title'] = 'Názov bloku';
 $lang['g002_apply'] = 'Použiť';
@@ -51,14 +51,14 @@ $lang['g002_sectionslist'] = 'Bloky menu';
 $lang['g002_yesno_y'] = 'Áno';
 $lang['g002_yesno_n'] = 'Nie';
 
-$lang['g002_owner'] = 'Majiteľ';
+$lang['g002_owner'] = 'Vlastník';
 $lang['g002_sectionid'] = 'Identifikátor';
 $lang['g002_name'] = 'Označenie';
 
 $lang['g002_configrandompic'] = 'Nastavenie náhodných fotografií';
 $lang['g002_setting_randompic_aboutpicture'] = 'Zobrazené údaje';
-$lang['g002_setting_randompic_showname'] = 'Ukázať názov fotografie';
-$lang['g002_setting_randompic_showcomment'] = 'Ukázať komentár fotografie';
+$lang['g002_setting_randompic_showname'] = 'Zobraziť názov fotografie';
+$lang['g002_setting_randompic_showcomment'] = 'Zobraziť komentár fotografie';
 $lang['g002_show_n'] = 'Nie';
 $lang['g002_show_o'] = 'Zobraziť nad fotkou';
 $lang['g002_show_u'] = 'Zobraziť pod fotkou';
@@ -80,7 +80,7 @@ $lang['g002_setting_personalised_nfo'] = 'Popis';
 $lang['g002_all_languages'] = "Všetky jazyky";
 
 $lang['g002_modmenu'] = '\'menu\' blok';
-$lang['g002_modspecial'] = '\'špeciálny\' block';
+$lang['g002_modspecial'] = '\'špeciálny\' blok';
 $lang['g002_labelmenu'] = 'Názov menu';
 
 $lang['g002_setting_randompic_periodicchange'] = 'Pravidelná zmena obrázku';
@@ -94,7 +94,7 @@ $lang['g002_cancel'] = 'Zrušiť';
 $lang['g002_piwigo_default'] = 'Piwigo predvolené zoraďovanie';
 $lang['g002_apply_changes'] = 'Použiť zmeny';
 $lang['g002_config_saved'] = 'Konfigurácia je uložená';
-$lang['g002_adviser_not_allowed'] = 'Poradca nie je povolený na vytvorenie zmien';
+$lang['g002_adviser_not_allowed'] = 'Poradca nemá povolenie na vykonávanie zmien';
 $lang['g002_click_to_manage_rights'] = 'Práva manažmentu';
 
 
@@ -103,21 +103,19 @@ $lang['g002_ok'] = "OK";
 $lang['g002_config_saved'] = "Konfigurácia uložená";
 $lang['g002_config_not_saved'] = "Konfigurácia nebola uložená";
 $lang['g002_selected_all_gallery'] = "Všetky albumy";
-$lang['g002_selected_favorites_wm'] = "Webmastrové najobľúbenejšie fotografie";
+$lang['g002_selected_favorites_wm'] = 'Najobľúbenejšie fotografie správcu webu';
 $lang['g002_selected_categories'] = "Všetky označené albumy";
 $lang['g002_loading'] = "Nahrávam...";
 $lang['g002_valid_order'] = "Pridaj zoradenie";
-$lang['g002_reset_order'] = "Zruš zoradenie";
+$lang['g002_reset_order'] = 'Zrušiť zoradenie';
 $lang['g002_accessibility'] = "Prípustnosť";
-$lang['g002_setting_core_blocks_content'] = "Obsah pre 'Menu' & 'Špeciálne menu";
-$lang['g002_setting_blocks_position'] = "Viditeľnosť & zoradenie pre menu ";
+$lang['g002_setting_core_blocks_content'] = 'Obsah \'Menu\' & \'Špeciálneho\' menu';
+$lang['g002_setting_blocks_position'] = 'Viditeľnosť a zoradenie pre menu ';
 $lang['g002_plugin'] = "Doplnok";
 $lang['g002_selectedpict'] = "Označené fotografie";
 $lang['g002_album_nfo'] = 'Povoliť konvertovanie jedného alebo viacerých albumov do menu';
 $lang['g002_album2menu'] = "Konvertovaný album do menu";
 $lang['g002_album'] = "Album &rArr; menu";
 $lang['g002_setting_albums_menus'] = "Albumy na konvertovanie do menu";
-$lang['gmaps_gpc_not_up_to_date']="Pre doplnok je potrebné mať inštalovaný <i>Grum Plugin Classes</i> verzie %s.
-Momentálne je inštalovaná verzia %s: urobte update na novú verziu doplnku <i>Grum Plugin Classes</i>.";
-
-?>
+$lang['gmaps_gpc_not_up_to_date'] = 'Pre doplnok je potrebné mať inštalovaný <i>Grum Plugin Classes</i> verzie %s.
+Momentálne je inštalovaná verzia %s: prejdite na novú verziu doplnku <i>Grum Plugin Classes</i>.';
