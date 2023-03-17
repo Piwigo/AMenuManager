d519e884196c0e32d03f4641af50d65504606b27
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-03-17 17:40:40 +0100

    Update fr_CA, thanks to bgoutier

---
+++ language/fr_CA/plugin.lang.php
@@ -0,0 +1,21 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['g002_setting_block_active'] = 'Afficher le bloc dans le menu';
+$lang['g002_setting_block_title'] = 'Titre du bloc dans le menu';
+$lang['g002_setting_personalised_content'] = 'Contenu du bloc';
+$lang['g002_nosections'] = 'Pas de blocs';
+$lang['g002_personalisedlist'] = 'Liste des blocs personnalisés';
+$lang['g002_section'] = 'bloc';
+$lang['g002_sections'] = 'blocs';
+$lang['g002_sectionslist'] = 'Bloc du menu';
+$lang['g002_selected_favorites_wm'] = 'Les photos favorites du webmestre';
+$lang['g002_createofpersonalised'] = 'Ajout d\'un bloc personnalisé';
+$lang['g002_editofpersonalised'] = 'Modification d\'un bloc personnalisé';
+$lang['g002_modmenu'] = 'Bloc \'menu\'';
+$lang['g002_modspecial'] = 'Bloc \'spéciale\'';
+$lang['g002_addsection'] = 'Ajouter un bloc';
